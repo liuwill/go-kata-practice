03e95cf38a67c72294c9d82c85b749d0c1ea33b6
liuwill <liuwei_will@qq.com>
2018-05-07 16:51:14 +0800

    check column for sudoku succedd

--- sudoku/validator.go
+++ sudoku/validator.go
@@ -18,7 +18,25 @@ func checkLine(board [][]byte) bool {
 	return true
 }
 
-func checkColumn(board [][]byte) bool { return false }
+func checkColumn(board [][]byte) bool {
+	for i := 0; i < 9; i++ {
+		counter := []int{0, 0, 0, 0, 0, 0, 0, 0, 0}
+		for j := 0; j < 9; j++ {
+			item := board[j][i]
+
+			if item == '.' {
+				continue
+			}
+
+			pos := item - 49
+			if counter[pos] > 0 {
+				return false
+			}
+			counter[pos] = 1
+		}
+	}
+	return true
+}
 
 func checkBlock(board [][]byte) bool { return false }
 
