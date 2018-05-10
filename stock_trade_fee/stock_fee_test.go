662a40d3c02042cc1995b69723740156282fb9c1
liuwill <liuwei_will@qq.com>
2018-05-10 10:06:51 +0800

    coverage more stock wiith fee

--- stock_trade_fee/stock_fee_test.go
+++ stock_trade_fee/stock_fee_test.go
@@ -16,3 +16,16 @@ func Test_maxProfitFee(t *testing.T) {
 
 	t.Log("Test_maxProfitFee pass")
 }
+
+func Test_maxProfitFeeCase(t *testing.T) {
+	prices := []int{1, 3, 4, 2, 8, 4, 9}
+	fee := 2
+	myProfit := maxProfit(prices, fee)
+	targetProfit := 8
+
+	if myProfit != targetProfit {
+		t.Error("get max profit fee case fail")
+	}
+
+	t.Log("Test_maxProfitFeeCase pass")
+}
