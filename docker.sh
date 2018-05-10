dd6e85351168a12e1d9075539daeef4c6dd8a8fd
liuwill <liuwei_will@qq.com>
2018-05-10 17:20:25 +0800

    generate gitlab coverage report html

--- docker.sh
+++ docker.sh
@@ -3,6 +3,6 @@
 DEMO_PATH=`pwd`
 
 docker run -itd \
-  -v $DEMO_PATH:/builds/liuwill/go-kata-practice \
+  -v $DEMO_PATH:/go/src/go-kata-practice \
   --name golang-kata \
   golang:1.10 &> /dev/null
--- tools/coverage.sh
+++ tools/coverage.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 #
 # Code coverage generation
-go test -covermode=count -coverprofile=cover.out ./...
+rm -rf dist
+mkdir dist
 
+go test -covermode=count -coverprofile=./dist/cover.out ./...
 
 # Display the global code coverage
-go tool cover -func=cover.out ;
+go tool cover -func=./dist/cover.out ;
 
 # If needed, generate HTML report
 if [ "$1" == "html" ]; then
-    go tool cover -html=cover.out -o coverage.html ;
+  go tool cover -html=./dist/cover.out -o ./dist/coverage.html ;
+  cp ./dist/coverage.html ./dist/index.html
 fi
