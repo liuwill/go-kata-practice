79b624549691bb45c070b9f693e0318ddf0ad94a
liuwill <liuwei_will@qq.com>
2018-05-10 14:06:38 +0800

    stock trade fee coverage 100%

--- stock_trade_fee/stock_fee_test.go
+++ stock_trade_fee/stock_fee_test.go
@@ -4,6 +4,30 @@ import (
 	"testing"
 )
 
+func Test_FeeAccount(t *testing.T) {
+	myAccount := Account{
+		fee:    2,
+		profit: 0,
+		cost:   0,
+		hold:   false,
+	}
+
+	myAccount.detectCost(3)
+	if myAccount.cost != 3 {
+		t.Error("test fee account fail")
+	}
+	myAccount.detectCost(4)
+	if myAccount.cost == 4 {
+		t.Error("test fee account fail")
+	}
+	myAccount.detectCost(2)
+	if myAccount.cost != 2 {
+		t.Error("test fee account fail")
+	}
+
+	t.Log("Test_FeeAccount pass")
+}
+
 func Test_maxProfitFee(t *testing.T) {
 	prices := []int{1, 3, 2, 8, 4, 9}
 	fee := 2
