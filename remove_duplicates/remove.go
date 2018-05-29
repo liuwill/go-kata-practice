d276ec16bb24e4e08b7e5b2c9a5a84f27f6ac101
liuwill <liuwei_will@qq.com>
2018-05-29 15:36:46 +0800

    just pass simple

--- remove_duplicates/remove.go
+++ remove_duplicates/remove.go
@@ -1,5 +1,13 @@
 package remove_duplicates
 
 func removeDuplicates(nums []int) int {
-	return len(nums)
+	length := 1
+	start := nums[1]
+	for i := 1; i < len(nums); i++ {
+		if nums[i] != start {
+			length++
+		}
+		start = nums[i]
+	}
+	return length
 }
