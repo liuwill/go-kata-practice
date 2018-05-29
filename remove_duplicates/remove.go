931c405ec8dfa0d88d1d82dbb2d86d21b8173ae9
liuwill <liuwei_will@qq.com>
2018-05-29 16:02:32 +0800

    it sames work but never happen

--- remove_duplicates/remove.go
+++ remove_duplicates/remove.go
@@ -3,9 +3,17 @@ package remove_duplicates
 func removeDuplicates(nums []int) int {
 	length := 1
 	start := nums[0]
+	pos := 0
 	for i := 1; i < len(nums); i++ {
 		if nums[i] != start {
 			length++
+			pos = i
+		}
+
+		if pos != i {
+			nums = append(nums[:i], nums[i+1:]...)
+
+			i--
 		}
 		start = nums[i]
 	}
