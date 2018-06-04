ac9860dc2463fb9344303d32c7ca5ed9174d9fe7
liuwill <liuwei_will@qq.com>
2018-06-04 19:59:00 +0800

    finish house robber

---
+++ house_robber/steal.go
@@ -0,0 +1,30 @@
+package house_robber
+
+func rob(nums []int) int {
+	length := len(nums)
+	for index := length - 1; index >= 0; index++ {
+		left := index + 1
+		right := index + 2
+
+		bigger := 0
+		if right < length && nums[right] > bigger {
+			bigger = nums[right]
+		}
+
+		if left < length && nums[left] > bigger {
+			bigger = nums[left]
+		}
+		nums[index] = nums[index] + bigger
+	}
+
+	result := 0
+	if length >= 1 {
+		result = nums[0]
+	}
+
+	if length > 1 && nums[1] > nums[0] {
+		result = nums[1]
+	}
+
+	return result
+}
