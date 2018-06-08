179f64a6725a87b5e68ddecebd777dc6dbff7f6f
liuwill <liuwei_will@qq.com>
2018-06-08 19:03:31 +0800

    finish code for keys and rooms

--- keys_rooms/enter.go
+++ keys_rooms/enter.go
@@ -6,14 +6,37 @@ type Room struct {
 }
 
 func canVisitAllRooms(rooms [][]int) bool {
-	// roomMap := make([]Room, len(rooms))
+	roomMap := make([]Room, len(rooms))
 	keys := []int{}
-	visited := 0
+	visited := 1
 
 	initkeys := rooms[0]
 	keys = append(keys, initkeys...)
-	for len(keys) > 0 && visited < len(rooms) {
+	roomMap[0] = Room{
+		Visited: true,
+		HasKey:  true,
+	}
 
+	for len(keys) > 0 && visited < len(rooms) {
+		index := keys[0]
+		keys = keys[1:]
+
+		current := roomMap[index]
+		roomMap[index].HasKey = true
+		if current.Visited {
+			continue
+		}
+
+		roomMap[index].Visited = true
+		currentKeys := rooms[index]
+		for _, key := range currentKeys {
+			keyRoom := roomMap[key]
+			if keyRoom.Visited || keyRoom.HasKey {
+				continue
+			}
+			roomMap[key].HasKey = true
+			keys = append(keys, key)
+		}
 	}
 
 	return visited == len(rooms)
