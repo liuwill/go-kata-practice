9ef205d5b4a6b5147832ca12b812ceb57e9a7b9c
liuwill <liuwei_will@qq.com>
2018-06-08 18:49:51 +0800

    finish basic framework

--- keys_rooms/enter.go
+++ keys_rooms/enter.go
@@ -1,5 +1,20 @@
 package keys_rooms
 
-func canVisitAllRooms(rooms [][]int) bool {
+type Room struct {
+	Visited bool
+	HasKey  bool
+}
 
+func canVisitAllRooms(rooms [][]int) bool {
+	// roomMap := make([]Room, len(rooms))
+	keys := []int{}
+	visited := 0
+
+	initkeys := rooms[0]
+	keys = append(keys, initkeys...)
+	for len(keys) > 0 && visited < len(rooms) {
+
+	}
+
+	return visited == len(rooms)
 }
