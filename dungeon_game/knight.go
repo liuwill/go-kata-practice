7c3e347956494685522c99626712c86039fe87c9
liuwill <liuwei_will@qq.com>
2018-06-09 16:54:50 +0800

    make structure of algorithm full

--- dungeon_game/knight.go
+++ dungeon_game/knight.go
@@ -13,22 +13,25 @@ func (action *Action) isBetterAction(x int, y int, marks [][]int) bool {
 	return false
 }
 
-func countEnergy(point Action, x int, y int, dungeon [][]int, marks [][]int) {
-
+func (action *Action) countEnergy(x int, y int, dungeon [][]int) int {
+	currentState := dungeon[x][y]
+	return action.Energy - currentState
 }
 
 func (action *Action) nextActions(width int, height int, dungeon [][]int, marks [][]int) []Action {
 	target := []Action{}
 	if action.X+1 < width && action.isBetterAction(action.X+1, action.Y, marks) {
 		target = append(target, Action{
-			X: action.X + 1,
-			Y: action.Y,
+			X:      action.X + 1,
+			Y:      action.Y,
+			Energy: action.countEnergy(action.X+1, action.Y, dungeon),
 		})
 	}
 	if action.Y+1 < height && action.isBetterAction(action.X, action.Y+1, marks) {
 		target = append(target, Action{
-			X: action.X,
-			Y: action.Y + 1,
+			X:      action.X,
+			Y:      action.Y + 1,
+			Energy: action.countEnergy(action.X, action.Y+1, dungeon),
 		})
 	}
 	return target
@@ -40,5 +43,15 @@ func calculateMinimumHP(dungeon [][]int) int {
 		pointMap[index] = make([]int, len(line))
 	}
 
-	return 0
+	actions := []Action{Action{
+		X:      0,
+		Y:      0,
+		Energy: -dungeon[0][0],
+	}}
+	best := 0
+	for len(actions) > 0 {
+
+	}
+
+	return best
 }
