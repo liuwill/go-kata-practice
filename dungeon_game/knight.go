632597a25d02355e02ba204e3a5ba9baad02847d
liuwill <liuwei_will@qq.com>
2018-06-09 17:04:39 +0800

    finish dungeon game algorihm coding

--- dungeon_game/knight.go
+++ dungeon_game/knight.go
@@ -21,26 +21,42 @@ func (action *Action) countEnergy(x int, y int, dungeon [][]int) int {
 func (action *Action) nextActions(width int, height int, dungeon [][]int, marks [][]int) []Action {
 	target := []Action{}
 	if action.X+1 < width && action.isBetterAction(action.X+1, action.Y, marks) {
+		xEnergy := action.countEnergy(action.X+1, action.Y, dungeon)
+		marks[action.X+1][action.Y] = xEnergy
 		target = append(target, Action{
 			X:      action.X + 1,
 			Y:      action.Y,
-			Energy: action.countEnergy(action.X+1, action.Y, dungeon),
+			Energy: xEnergy,
 		})
 	}
 	if action.Y+1 < height && action.isBetterAction(action.X, action.Y+1, marks) {
+		yEnergy := action.countEnergy(action.X, action.Y+1, dungeon)
+		marks[action.X+1][action.Y] = yEnergy
 		target = append(target, Action{
 			X:      action.X,
 			Y:      action.Y + 1,
-			Energy: action.countEnergy(action.X, action.Y+1, dungeon),
+			Energy: yEnergy,
 		})
 	}
 	return target
 }
 
+func (action *Action) isEnd(width int, height int) bool {
+	if action.X == width-1 && action.Y == height-1 {
+		return true
+	}
+	return false
+}
+
 func calculateMinimumHP(dungeon [][]int) int {
-	pointMap := make([][]int, len(dungeon))
+	marks := make([][]int, len(dungeon))
 	for index, line := range dungeon {
-		pointMap[index] = make([]int, len(line))
+		marks[index] = make([]int, len(line))
+	}
+	width := len(dungeon)
+	height := 0
+	if len(dungeon) > 0 {
+		height = len(dungeon[0])
 	}
 
 	actions := []Action{Action{
@@ -50,7 +66,13 @@ func calculateMinimumHP(dungeon [][]int) int {
 	}}
 	best := 0
 	for len(actions) > 0 {
+		currentAction := actions[0]
+		if currentAction.isEnd(width, height) && currentAction.Energy <= best {
+			best = currentAction.Energy
+			continue
+		}
 
+		currentAction.nextActions(width, height, dungeon, marks)
 	}
 
 	return best
