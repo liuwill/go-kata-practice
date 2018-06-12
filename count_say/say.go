b41e6bbe7e7a9c46d5dab36cd32ab86646d157e1
liuwill <liuwei_will@qq.com>
2018-06-12 14:00:44 +0800

    finish count say coding

--- count_say/say.go
+++ count_say/say.go
@@ -1,8 +1,41 @@
 package count_say
 
+import "strings"
+
+func say(letter string, count int) string {
+	return strings.Repeat(letter, count)
+}
+
+func read(input string) string {
+	last := ""
+	result := ""
+	count := 0
+
+	for index, v := range input {
+		letter := string(v)
+		if index == 0 {
+			count = 1
+			last = letter
+			continue
+		}
+
+		if last == letter {
+			count++
+		} else if last != letter {
+			result += say(last, count)
+			count = 1
+			last = letter
+		}
+	}
+	result += say(last, count)
+
+	return result
+}
+
 func countAndSay(n int) string {
 	result := "1"
 	for i := 2; i <= n; i++ {
+		result = read(result)
 	}
 
 	return result
