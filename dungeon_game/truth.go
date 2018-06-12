a7daed7db43c15f61221b1424d3384fc5d48f4bb
liuwill <liuwei_will@qq.com>
2018-06-12 19:00:22 +0800

    refactor to improve dungeon game truth

--- dungeon_game/truth.go
+++ dungeon_game/truth.go
@@ -14,6 +14,22 @@ func min(x int, y int) int {
 	return x
 }
 
+func countMarks(i int, j int, width int, height int, dungeon [][]int, marks [][]int) int {
+	mark := marks[i][j]
+	if i+1 <= height-1 && j+1 <= width-1 {
+		mark = min(marks[i+1][j], marks[i][j+1]) - dungeon[i][j]
+	} else if i+1 <= height-1 {
+		mark = marks[i+1][j] - dungeon[i][j]
+	} else if j+1 <= width-1 {
+		mark = marks[i][j+1] - dungeon[i][j]
+	}
+
+	if mark <= 0 {
+		mark = 1
+	}
+	return mark
+}
+
 func calculateMinimumHP(dungeon [][]int) int {
 	width := len(dungeon[0])
 	height := len(dungeon)
@@ -26,16 +42,7 @@ func calculateMinimumHP(dungeon [][]int) int {
 	marks[height-1][width-1] = max(-dungeon[height-1][width-1], 0) + 1
 	for i := height - 1; i >= 0; i-- {
 		for j := width - 1; j >= 0; j-- {
-			if i+1 <= height-1 && j+1 <= width-1 {
-				marks[i][j] = min(marks[i+1][j], marks[i][j+1]) - dungeon[i][j]
-			} else if i+1 <= height-1 {
-				marks[i][j] = marks[i+1][j] - dungeon[i][j]
-			} else if j+1 <= width-1 {
-				marks[i][j] = marks[i][j+1] - dungeon[i][j]
-			}
-			if marks[i][j] <= 0 {
-				marks[i][j] = 1
-			}
+			marks[i][j] = countMarks(i, j, width, height, dungeon, marks)
 		}
 	}
 
