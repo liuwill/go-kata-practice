9ae5dc61f662cb5cf3c560b9ba831b80cfdad72c
liuwill <liuwei_will@qq.com>
2018-06-12 18:34:54 +0800

    run jump game success

--- jump_game/can.go
+++ jump_game/can.go
@@ -1,5 +1,14 @@
 package jump_game
 
 func canJump(nums []int) bool {
+	target := len(nums) - 1
 
+	for i := len(nums) - 1; i >= 0; i-- {
+		current := nums[i]
+		if i+current >= target {
+			target = i
+		}
+	}
+
+	return target == 0
 }
