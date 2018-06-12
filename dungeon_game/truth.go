65ee66fb55896337017d1c57bdefe4914ee77c2c
liuwill <liuwei_will@qq.com>
2018-06-12 19:19:09 +0800

    reduce function arguments

--- dungeon_game/truth.go
+++ dungeon_game/truth.go
@@ -14,7 +14,10 @@ func min(x int, y int) int {
 	return x
 }
 
-func countMarks(i int, j int, width int, height int, dungeon [][]int, marks [][]int) int {
+func countMarks(i int, j int, dungeon [][]int, marks [][]int) int {
+	width := len(dungeon[0])
+	height := len(dungeon)
+
 	mark := marks[i][j]
 	if i+1 <= height-1 && j+1 <= width-1 {
 		mark = min(marks[i+1][j], marks[i][j+1]) - dungeon[i][j]
@@ -42,9 +45,40 @@ func calculateMinimumHP(dungeon [][]int) int {
 	marks[height-1][width-1] = max(-dungeon[height-1][width-1], 0) + 1
 	for i := height - 1; i >= 0; i-- {
 		for j := width - 1; j >= 0; j-- {
-			marks[i][j] = countMarks(i, j, width, height, dungeon, marks)
+			marks[i][j] = countMarks(i, j, dungeon, marks)
 		}
 	}
 
 	return marks[0][0]
 }
+
+/*
+func calculateMinimumHP(dungeon [][]int) int {
+	width := len(dungeon[0])
+	height := len(dungeon)
+
+	marks := make([][]int, len(dungeon))
+	for index, line := range dungeon {
+		marks[index] = make([]int, len(line))
+	}
+
+	marks[height-1][width-1] = max(-dungeon[height-1][width-1], 0) + 1
+	for i := height - 1; i >= 0; i-- {
+		for j := width - 1; j >= 0; j-- {
+			if i+1 <= height-1 && j+1 <= width-1 {
+        marks[i][j] = min(marks[i+1][j], marks[i][j+1]) - dungeon[i][j]
+      } else if i+1 <= height-1 {
+        marks[i][j] = marks[i+1][j] - dungeon[i][j]
+      } else if j+1 <= width-1 {
+        marks[i][j] = marks[i][j+1] - dungeon[i][j]
+      }
+
+      if marks[i][j] <= 0 {
+        marks[i][j] = 1
+      }
+		}
+	}
+
+	return marks[0][0]
+}
+*/
