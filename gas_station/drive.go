c27a0a00701b35853263d65f66e6144ccacff397
liuwill <liuwei_will@qq.com>
2018-06-13 19:43:13 +0800

    basic gas station structure

--- gas_station/drive.go
+++ gas_station/drive.go
@@ -1,7 +1,25 @@
 package gas_station
 
+func next(current int, length int) int {
+	return (current + 1) % length
+}
+
+func drive(start int, gas []int, cost []int) bool {
+	length := len(gas)
+	for i := 0; i < length; i++ {
+
+	}
+	return false
+}
+
 func canCompleteCircuit(gas []int, cost []int) int {
-	origin := 0
+	origin := -1
+	for i, _ := range gas {
+		if drive(i, gas, cost) {
+			origin = i
+			break
+		}
+	}
 
 	return origin
 }
