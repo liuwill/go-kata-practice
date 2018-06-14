10f20e5a006818f9ca7ffc3fd856e66b804b1ee4
liuwill <liuwei_will@qq.com>
2018-06-14 19:19:19 +0800

    make basic twitter structure

--- twitter/tweet.go
+++ twitter/tweet.go
@@ -1,10 +1,18 @@
 package twitter
 
 type Twitter struct {
+	Funs    map[int][]int
+	Followd map[int][]int
+	Tweets  map[int][]int
 }
 
 /** Initialize your data structure here. */
 func Constructor() Twitter {
+	return Twitter{
+		Funs:    make(map[int][]int),
+		Followd: make(map[int][]int),
+		Tweets:  make(map[int][]int),
+	}
 }
 
 /** Compose a new tweet. */
@@ -14,7 +22,10 @@ func (this *Twitter) PostTweet(userId int, tweetId int) {
 
 /** Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent. */
 func (this *Twitter) GetNewsFeed(userId int) []int {
-
+	if _, ok := this.Tweets[userId]; ok {
+		return this.Tweets[userId]
+	}
+	return []int{}
 }
 
 /** Follower follows a followee. If the operation is invalid, it should be a no-op. */
