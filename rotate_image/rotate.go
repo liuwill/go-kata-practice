77366cdd12c164e7c3dc052bcab8bfe8677a5093
liuwill <liuwei_will@qq.com>
2018-07-18 16:06:31 +0800

    maybe finish rotate image

--- rotate_image/rotate.go
+++ rotate_image/rotate.go
@@ -1,5 +1,18 @@
 package rotate_image
 
 func rotate(matrix [][]int) {
-
+	for i := 0; i < len(matrix)/2; i++ {
+		innerLen := len(matrix) - i - 1
+		for j := i; j < innerLen; j++ {
+			distance := len(matrix) - j - 1
+			if i == distance {
+				continue
+			}
+			temp := matrix[i][j]
+			matrix[i][j] = matrix[distance][j]
+			matrix[distance][j] = matrix[distance][distance]
+			matrix[distance][distance] = matrix[i][distance]
+			matrix[i][distance] = temp
+		}
+	}
 }
