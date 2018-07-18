81e78ee881830309b9e053c6a6aba2374f8e9a3f
liuwill <liuwei_will@qq.com>
2018-07-18 17:08:03 +0800

    simple example pass

--- rotate_image/rotate.go
+++ rotate_image/rotate.go
@@ -3,16 +3,19 @@ package rotate_image
 func rotate(matrix [][]int) {
 	for i := 0; i < len(matrix)/2; i++ {
 		innerLen := len(matrix) - i - 1
+		distance := len(matrix) - i - 1
+
 		for j := i; j < innerLen; j++ {
-			distance := len(matrix) - j - 1
-			if i == distance {
+			println(i, j, distance)
+			if j == distance && i == distance {
 				continue
 			}
+			println("++", matrix[i][j], matrix[distance-j][i], matrix[distance-i][distance-j], matrix[j][distance-i])
 			temp := matrix[i][j]
-			matrix[i][j] = matrix[distance][j]
-			matrix[distance][j] = matrix[distance][distance]
-			matrix[distance][distance] = matrix[i][distance]
-			matrix[i][distance] = temp
+			matrix[i][j] = matrix[distance-j][i]
+			matrix[distance-j][i] = matrix[distance-i][distance-j]
+			matrix[distance-i][distance-j] = matrix[j][distance-i]
+			matrix[j][distance-i] = temp
 		}
 	}
 }
