a5e8a4a4ed44047752495ef5c068facaf72bd5ad
liuwill <liuwei_will@qq.com>
2018-07-18 17:52:04 +0800

    cover more test case

--- rotate_image/rotate.go
+++ rotate_image/rotate.go
@@ -2,19 +2,18 @@ package rotate_image
 
 func rotate(matrix [][]int) {
 	for i := 0; i < len(matrix)/2; i++ {
-		innerLen := len(matrix) - i - 1
 		distance := len(matrix) - i - 1
 
-		for j := i; j < innerLen; j++ {
-			println(i, j, distance)
-			if j == distance && i == distance {
-				continue
-			}
+		for j := i; j < distance; j++ {
+			// if j == distance && i == distance {
+			// 	continue
+			// }
+
 			temp := matrix[i][j]
-			matrix[i][j] = matrix[distance-j][i]
-			matrix[distance-j][i] = matrix[distance-i][distance-j]
-			matrix[distance-i][distance-j] = matrix[j][distance-i]
-			matrix[j][distance-i] = temp
+			matrix[i][j] = matrix[distance-j+i][i]
+			matrix[distance-j+i][i] = matrix[distance][distance-j+i]
+			matrix[distance][distance-j+i] = matrix[j][distance]
+			matrix[j][distance] = temp
 		}
 	}
 }
--- rotate_image/rotate_test.go
+++ rotate_image/rotate_test.go
@@ -33,3 +33,24 @@ func Test_Rotate(t *testing.T) {
 	}
 	t.Log("Translate Test_Rotate Success")
 }
+
+func Test_RotateMore(t *testing.T) {
+	source := [][]int{
+		{5, 1, 9, 11},
+		{2, 4, 8, 10},
+		{13, 3, 6, 7},
+		{15, 14, 12, 16},
+	}
+	rotate(source)
+	expect := [][]int{
+		{15, 13, 2, 5},
+		{14, 3, 4, 1},
+		{12, 6, 8, 9},
+		{16, 7, 10, 11},
+	}
+
+	if !isArrayMatch(source, expect) {
+		t.Error("Translate rotate more Fail", source)
+	}
+	t.Log("Translate Test_RotateMore Success")
+}
