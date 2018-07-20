8fcd611b051bf445efa3f04be231b0fa2602aaa4
liuwill <liuwei_will@qq.com>
2018-07-20 11:10:20 +0800

    test sort color fail

--- sort_colors/sort.go
+++ sort_colors/sort.go
@@ -1,5 +1,13 @@
 package sort_colors
 
 func sortColors(nums []int) {
-
+	for i, _ := range nums {
+		for j := i - 1; j >= 0; j-- {
+			if nums[j] > nums[i] {
+				temp := nums[i]
+				nums[i] = nums[j]
+				nums[j] = temp
+			}
+		}
+	}
 }
---
+++ sort_colors/sort_test.go
@@ -0,0 +1,25 @@
+package sort_colors
+
+import (
+	"testing"
+)
+
+func isArrayMatch(target []int, expect []int) bool {
+	for i := 0; i < len(target); i++ {
+		if target[i] != expect[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func Test_Sort(t *testing.T) {
+	source := []int{2, 0, 2, 1, 1, 0}
+	sortColors(source)
+	expect := []int{0, 0, 1, 1, 2, 2}
+
+	if !isArrayMatch(source, expect) {
+		t.Error("Translate rotate Fail", source)
+	}
+	t.Log("Translate Test_Rotate Success")
+}
