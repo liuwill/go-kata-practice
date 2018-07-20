d402d7cd463075a359894d559487df76200865bf
liuwill <liuwei_will@qq.com>
2018-07-20 14:02:13 +0800

    simple pass sort colors

--- sort_colors/sort.go
+++ sort_colors/sort.go
@@ -1,12 +1,33 @@
 package sort_colors
 
+func changePos(nums []int, x int, y int) {
+	temp := nums[x]
+	nums[x] = nums[y]
+	nums[y] = temp
+}
+
 func sortColors(nums []int) {
+	whitePos := 0
+	bluePos := len(nums) - 1
+
 	for i, _ := range nums {
-		for j := i - 1; j >= 0; j-- {
-			if nums[j] > nums[i] {
-				temp := nums[i]
-				nums[i] = nums[j]
-				nums[j] = temp
+		if nums[i] == 0 {
+			for j := whitePos; j < i; j++ {
+				if nums[j] > nums[i] {
+					changePos(nums, i, j)
+					whitePos = j
+					break
+				}
+			}
+		}
+
+		if nums[i] == 2 {
+			for j := bluePos; j > i; j-- {
+				if nums[j] < nums[i] {
+					changePos(nums, i, j)
+					bluePos = j
+					break
+				}
 			}
 		}
 	}
