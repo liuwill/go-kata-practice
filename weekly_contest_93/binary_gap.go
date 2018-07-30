c102bb4f1cfcebafaac4c73dce6a9519776732f4
liuwill <liuwei_will@qq.com>
2018-07-30 10:46:47 +0800

    pass all test case for banary gap

--- weekly_contest_93/binary_gap.go
+++ weekly_contest_93/binary_gap.go
@@ -1,20 +1,25 @@
 package weekly_contest_93
 
 func binaryGap(N int) int {
+	// println("------", N)
 	num := N
 	result := 0
 	length := 0
+	start := 0
 	for num > 0 {
 		current := num % 2
 		num = num / 2
-		if current == 1 {
-			length = 0
-		} else {
-			length++
-		}
 
-		if length > result {
-			result = length
+		// println(current, length, start, result)
+		if current == 1 {
+			if length+1 > result && start == 1 {
+				result = length + 1
+				start = 0
+			}
+			start = 1
+			length = 0
+		} else if current == 0 && start == 1 {
+			length++
 		}
 	}
 
