323d9af17902f7362d7ee62d88cbe6ec6dac0539
liuwill <liuwei_will@qq.com>
2018-08-13 17:20:50 +0800

    find a simple solution

--- weekly_contest_97/super_egg_drop.go
+++ weekly_contest_97/super_egg_drop.go
@@ -2,9 +2,18 @@ package weekly_contest_97
 
 func superEggDrop(K int, N int) int {
 	times := 0
-	for N > 0 {
+
+	tail := N
+	left := K
+
+	for K > 1 && tail > 0 {
+		tail = tail / 2
 		times++
-		N = N / 2
+		left--
+	}
+
+	if K == 1 && tail > 0 {
+		times = times + tail
 	}
 
 	return times
