95259c4a67979ccc6d01515218db5e1c32668684
liuwill <liuwei_will@qq.com>
2018-08-14 14:34:19 +0800

    first uncommon word answer

--- weekly_contest_97/uncommon_words.go
+++ weekly_contest_97/uncommon_words.go
@@ -1,5 +1,37 @@
 package weekly_contest_97
 
 func uncommonFromSentences(A string, B string) []string {
-	return nil
+	dict := make(map[string]int)
+	word := ""
+	for _, v := range A {
+		letter := string(v)
+
+		if letter != " " {
+			word += letter
+		} else {
+			if _, ok := dict[word]; ok {
+				dict[word]++
+			} else {
+				dict[word] = 1
+			}
+			word = ""
+		}
+	}
+
+	if len(word) > 0 {
+		if _, ok := dict[word]; ok {
+			dict[word]++
+		} else {
+			dict[word] = 1
+		}
+	}
+
+	result := []string{}
+	for k, v := range dict {
+		if v == 1 {
+			result = append(result[:], k)
+		}
+	}
+
+	return result
 }
