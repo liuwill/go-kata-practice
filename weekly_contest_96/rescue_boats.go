07293f59a536fad0804ed64c1ab97abea36469e5
liuwill <liuwei_will@qq.com>
2018-08-15 10:50:33 +0800

    solve last item solo problem

--- weekly_contest_96/rescue_boats.go
+++ weekly_contest_96/rescue_boats.go
@@ -33,5 +33,9 @@ func numRescueBoats(people []int, limit int) int {
 		}
 		boat = 0
 	}
+
+	if people[index] > 0 {
+		times++
+	}
 	return times
 }
--- weekly_contest_96/rescue_boats_test.go
+++ weekly_contest_96/rescue_boats_test.go
@@ -15,3 +15,27 @@ func Test_NumRescueBoats(t *testing.T) {
 	}
 	t.Log("Translate Test_NumRescueBoats Success")
 }
+
+func Test_NumRescueBoatsLess(t *testing.T) {
+	source := []int{1, 2}
+	limit := 3
+	target := numRescueBoats(source, limit)
+	expect := 1
+
+	if expect != target {
+		t.Error("Translate Test_NumRescueBoatsLess Fail", target)
+	}
+	t.Log("Translate Test_NumRescueBoatsLess Success")
+}
+
+func Test_NumRescueBoatsMore(t *testing.T) {
+	source := []int{3, 5, 3, 4}
+	limit := 5
+	target := numRescueBoats(source, limit)
+	expect := 4
+
+	if expect != target {
+		t.Error("Translate Test_NumRescueBoatsMore Fail", target)
+	}
+	t.Log("Translate Test_NumRescueBoatsMore Success")
+}
