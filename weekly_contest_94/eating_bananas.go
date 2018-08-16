7b38abbedf6cb64f1fdd7d5eb7a80ed0826c25ad
liuwill <liuwei_will@qq.com>
2018-08-16 13:36:43 +0800

    code down

--- weekly_contest_94/eating_bananas.go
+++ weekly_contest_94/eating_bananas.go
@@ -1,5 +1,47 @@
 package weekly_contest_94
 
+func countEatableHour(K int, piles []int) int {
+	hours := 0
+	for _, v := range piles {
+		hours += v / K
+		if v%K > 0 {
+			hours++
+		}
+	}
+
+	return hours
+}
+
 func minEatingSpeed(piles []int, H int) int {
-	return 0
+	largest := piles[0]
+	smallest := piles[0]
+
+	for _, v := range piles {
+		if v > largest {
+			largest = v
+		}
+
+		if v < smallest {
+			smallest = v
+		}
+	}
+
+	middle := (largest + smallest) / 2
+	top := smallest
+	bottom := largest
+	answer := 0
+	for true {
+		current := countEatableHour(middle, piles)
+		if current > H {
+			top = middle
+		} else {
+			bottom = middle
+			if middle < answer || answer == 0 {
+				answer = middle
+			}
+		}
+
+		middle = (top + bottom) / 2
+	}
+	return answer
 }
