4e7622644b27fcedbdc25410bfa4d47279a137ec
liuwill <liuwei_will@qq.com>
2018-08-16 14:04:10 +0800

    improve code

--- weekly_contest_96/rescue_boats_sort.go
+++ weekly_contest_96/rescue_boats_sort.go
@@ -20,12 +20,14 @@ func numRescueBoatsSort(people []int, limit int) int {
 			// fmt.Printf("i=%v boat=%v index=%v %v -> %v\n", i, boat, index, limit, times)
 			if people[i] == 0 {
 				continue
-			} else if boat == 0 {
+			}
+
+			if boat == 0 {
 				boat += people[i]
 				people[i] = 0
 				pos = i
 				number = 1
-			} else if boat+people[i] <= limit {
+			} else if boat > 0 && boat+people[i] <= limit {
 				boat += people[i]
 				people[i] = 0
 				number++
