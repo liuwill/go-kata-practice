a5c2959a72c8a0c6ba09668c8755e9897001dc92
liuwill <liuwei_will@qq.com>
2018-08-17 00:23:40 +0800

    fix max triangle test

--- weekly_contest_79/largest_triangle_area_test.go
+++ weekly_contest_79/largest_triangle_area_test.go
@@ -1,15 +1,20 @@
 package weekly_contest_79
 
 import (
+	"math"
 	"testing"
 )
+
+func equals(expect float64, target float64) bool {
+	return math.Round(expect*1000000) == math.Round(target*1000000)
+}
 
 func Test_LargestTriangleArea(t *testing.T) {
 	source := [][]int{{0, 0}, {0, 1}, {1, 0}, {0, 2}, {2, 0}}
 	target := largestTriangleArea(source)
 	expect := 2.0
 
-	if expect != target {
+	if !equals(expect, target) {
 		t.Error("Translate Test_LargestTriangleArea Fail", target)
 	}
 	t.Log("Translate Test_LargestTriangleArea Success")
