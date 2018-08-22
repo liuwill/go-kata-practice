8405958b063aca42814c2e3ca485548476bc226e
liuwill <liuwei_will@qq.com>
2018-08-22 09:59:15 +0800

    coding basic frame but test fail

--- weekly_contest_98/find_replace_pattern.go
+++ weekly_contest_98/find_replace_pattern.go
@@ -1,5 +1,18 @@
 package weekly_contest_98
 
+import "strings"
+
+func comparePattern(content string, pattern string) bool {
+	return strings.Compare(content, pattern) == 0
+}
+
 func findAndReplacePattern(words []string, pattern string) []string {
-	return nil
+	result := []string{}
+
+	for _, value := range words {
+		if comparePattern(value, pattern) {
+			result = append(result, value)
+		}
+	}
+	return result
 }
---
+++ weekly_contest_98/find_replace_pattern_test.go
@@ -0,0 +1,28 @@
+package weekly_contest_98
+
+import "testing"
+
+func compareStringList(source []string, target []string) bool {
+	if len(source) != len(target) {
+		return false
+	}
+
+	for index := 0; index < len(source); index++ {
+		if source[index] != target[index] {
+			return false
+		}
+	}
+
+	return true
+}
+func Test_FindAndReplacePattern(t *testing.T) {
+	words := []string{"abc", "deq", "mee", "aqq", "dkd", "ccc"}
+	pattern := "abb"
+	target := findAndReplacePattern(words, pattern)
+	expect := []string{"mee", "aqq"}
+
+	if !compareStringList(expect, target) {
+		t.Error("Translate Test_FindAndReplacePattern Fail", target)
+	}
+	t.Log("Translate Test_FindAndReplacePattern Success")
+}
