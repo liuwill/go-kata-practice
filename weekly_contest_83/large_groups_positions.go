b94dc4415bb525cf559ab786a3214f92f7c371fe
liuwill <liuwei_will@qq.com>
2018-08-29 19:50:47 +0800

    solve the problem

--- weekly_contest_83/large_groups_positions.go
+++ weekly_contest_83/large_groups_positions.go
@@ -1,5 +1,26 @@
 package weekly_contest_83
 
 func largeGroupPositions(S string) [][]int {
-	return nil
+	target := [][]int{}
+	current := '#'
+	start := 0
+	end := 0
+	for i, v := range S {
+		if v == current {
+			end = i
+			continue
+		}
+
+		if end-start >= 3 {
+			target = append(target, []int{start, end})
+		}
+		current = v
+		start = i
+		end = i
+	}
+
+	if end-start >= 3 {
+		target = append(target, []int{start, end})
+	}
+	return target
 }
