137e3daa810de3d0f31db00ae31f4863a90f4345
liuwill <liuwei_will@qq.com>
2018-08-29 19:43:06 +0800

    coding test utils

--- weekly_contest_83/large_groups_positions_test.go
+++ weekly_contest_83/large_groups_positions_test.go
@@ -2,8 +2,30 @@ package weekly_contest_83
 
 import "testing"
 
+func compareList(source []int, target []int) bool {
+	if len(source) != len(target) {
+		return false
+	}
+
+	for index := 0; index < len(source); index++ {
+		if source[index] != target[index] {
+			return false
+		}
+	}
+
+	return true
+}
+
 func deepCompare(expect [][]int, target [][]int) bool {
-	return false
+	if len(expect) != len(target) {
+		return false
+	}
+	for i := 0; i < len(expect); i++ {
+		if !compareList(expect[i], target[i]) {
+			return false
+		}
+	}
+	return true
 }
 
 func Test_LargeGroupPositions(t *testing.T) {
@@ -13,7 +35,7 @@ func Test_LargeGroupPositions(t *testing.T) {
 		{3, 6},
 	}
 
-	if !deepCompare(expect, target) {
+	if target == nil || !deepCompare(expect, target) {
 		t.Error("Translate Test_LargeGroupPositions Fail", target)
 	}
 	t.Log("Translate Test_LargeGroupPositions Success")
