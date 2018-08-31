0e587e8faa73b0bcec8a6bc29a5435cd458981d3
liuwill <liuwei_will@qq.com>
2018-08-31 14:22:59 +0800

    pass init case

--- daily_challenge/dota2_senate.go
+++ daily_challenge/dota2_senate.go
@@ -15,18 +15,28 @@ func predictPartyVictory(senate string) string {
 		"D": "R",
 	}
 
-	for _, v := range senate {
-		side := string(v)
-		if voteCount[side] > 0 {
-			voteCount[side]--
-		} else {
-			other := otherMap[side]
-			voteCount[other]++
+	left := senate
+	victory := ""
+	for len(victory) == 0 {
+		current := ""
+		for _, v := range left {
+			side := string(v)
+			if voteCount[side] > 0 {
+				voteCount[side]--
+			} else {
+				current += side
+				other := otherMap[side]
+				voteCount[other]++
+			}
 		}
+
+		if voteCount["R"] < voteCount["D"] {
+			victory = Radiant
+		} else if voteCount["R"] > voteCount["D"] {
+			victory = Dire
+		}
+		left = current
 	}
 
-	if voteCount["R"] < voteCount["D"] {
-		return Radiant
-	}
-	return Dire
+	return victory
 }
