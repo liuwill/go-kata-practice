c7838a63878230ca34fe6933d722b74c1c4d5912
liuwill <liuwei_will@qq.com>
2018-09-03 18:56:27 +0800

    finish init code

--- weekly_contest_99/surface_area.go
+++ weekly_contest_99/surface_area.go
@@ -1,5 +1,57 @@
 package weekly_contest_99
 
-func surfaceArea(grid [][]int) int {
+func countFront(grid [][]int, i int, j int) int {
+	if i-1 < 0 {
+		return grid[i][j]
+	} else if grid[i-1][j] < grid[i][j] {
+		return grid[i][j] - grid[i-1][j]
+	}
+
 	return 0
+}
+
+func countBackend(grid [][]int, i int, j int) int {
+	if i+1 == len(grid) {
+		return grid[i][j]
+	} else if grid[i+1][j] < grid[i][j] {
+		return grid[i][j] - grid[i+1][j]
+	}
+
+	return 0
+}
+
+func countTop(grid [][]int, i int, j int) int {
+	if j+1 == len(grid) {
+		return grid[i][j]
+	} else if grid[i][j+1] < grid[i][j] {
+		return grid[i][j] - grid[i][j+1]
+	}
+
+	return 0
+}
+
+func countDown(grid [][]int, i int, j int) int {
+	if j-1 < len(grid) {
+		return grid[i][j]
+	} else if grid[i][j-1] < grid[i][j] {
+		return grid[i][j] - grid[i][j-1]
+	}
+
+	return 0
+}
+
+func surfaceArea(grid [][]int) int {
+	result := 0
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid); j++ {
+			topBottom := 2
+			result += topBottom
+
+			result += countFront(grid, i, j)
+			result += countBackend(grid, i, j)
+			result += countTop(grid, i, j)
+			result += countDown(grid, i, j)
+		}
+	}
+	return result
 }
