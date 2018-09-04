39e60b3ca8ff6688812c55ce5292736edfe582f9
liuwill <liuwei_will@qq.com>
2018-09-04 17:31:42 +0800

    first try simple test

--- weekly_contest_98/subsequence_width.go
+++ weekly_contest_98/subsequence_width.go
@@ -1,5 +1,22 @@
 package weekly_contest_98
 
 func sumSubseqWidths(A []int) int {
-	return len(A)
+	sum := 0
+
+	for i := 0; i < len(A); i++ {
+		max := A[i]
+		min := A[i]
+		for j := i + 1; j < len(A); j++ {
+			if min > A[j] {
+				min = A[j]
+			}
+			if max < A[j] {
+				max = A[j]
+			}
+
+			sum += max - min
+		}
+	}
+
+	return sum
 }
