d3d1c16a97a8318a5968d6944d13a1933a7107ac
liuwill <liuwei_will@qq.com>
2018-09-06 09:59:10 +0800

    finish coding

--- daily_challenge/two_sum.go
+++ daily_challenge/two_sum.go
@@ -3,5 +3,18 @@ package daily_challenge
 func twoSum(nums []int, target int) []int {
 	result := make([]int, 2)
 
+	find := false
+	for i := 0; i < len(nums)-1 && !find; i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] != target {
+				continue
+			}
+
+			result[0] = nums[i]
+			result[1] = nums[j]
+			find = true
+		}
+	}
+
 	return result
 }
