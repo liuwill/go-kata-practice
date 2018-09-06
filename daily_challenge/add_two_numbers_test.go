0de7eee7435e4d0e2ffe2e5c2c157b29700e2045
liuwill <liuwei_will@qq.com>
2018-09-06 15:48:35 +0800

    build test utils for ListNode

--- daily_challenge/add_two_numbers_test.go
+++ daily_challenge/add_two_numbers_test.go
@@ -25,15 +25,63 @@ func compareListNode(expect *ListNode, target *ListNode) bool {
 	return true
 }
 
+func transformNumberToListNode(number int) *ListNode {
+	if number == 0 {
+		return &ListNode{
+			Val:  0,
+			Next: nil,
+		}
+	}
+
+	current := &ListNode{
+		Val:  -1,
+		Next: nil,
+	}
+
+	result := current
+	for number > 0 {
+		bit := number % 10
+		number = number / 10
+
+		if current.Val == -1 {
+			current.Val = bit
+		} else {
+			current.Next = &ListNode{
+				Val:  bit,
+				Next: nil,
+			}
+			current = current.Next
+		}
+	}
+
+	return result
+}
+
+func printListNode(list *ListNode) []int {
+	result := []int{}
+
+	current := list
+	for current != nil {
+		result = append(result, current.Val)
+		current = current.Next
+	}
+
+	return result
+}
+
+func Test_TransformNumberToListNode(t *testing.T) {
+	list := transformNumberToListNode(342)
+	target := printListNode(list)
+	expect := []int{2, 4, 3}
+
+	if !compareList(expect, target) {
+		t.Error("Translate Test_TransformNumberToListNode Fail", target)
+	}
+}
+
 func Test_AddTwoNumbers(t *testing.T) {
-	source1 := &ListNode{
-		Val:  0,
-		Next: nil,
-	}
-	source2 := &ListNode{
-		Val:  0,
-		Next: nil,
-	}
+	source1 := transformNumberToListNode(0)
+	source2 := transformNumberToListNode(0)
 	target := addTwoNumbers(source1, source2)
 	expect := &ListNode{
 		Val:  0,
