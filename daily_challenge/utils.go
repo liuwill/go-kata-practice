667851694c045b2a6a9e74f17d3c63f53492b006
liuwill <liuwei_will@qq.com>
2018-09-06 10:01:17 +0800

    test for daily challenge utils

---
+++ daily_challenge/utils.go
@@ -0,0 +1,15 @@
+package daily_challenge
+
+func compareList(source []int, target []int) bool {
+	if len(source) != len(target) {
+		return false
+	}
+
+	for index := 0; index < len(source); index++ {
+		if source[index] != target[index] {
+			return false
+		}
+	}
+
+	return true
+}
