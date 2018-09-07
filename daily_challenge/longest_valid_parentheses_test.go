dd4a9f4df33355b7387e2d065919ebd3e2c5776d
liuwill <liuwei_will@qq.com>
2018-09-07 10:16:53 +0800

    add more test case for valid

--- daily_challenge/longest_valid_parentheses_test.go
+++ daily_challenge/longest_valid_parentheses_test.go
@@ -12,3 +12,36 @@ func Test_LongestValidParentheses(t *testing.T) {
 	}
 	t.Log("Translate Test_LongestValidParentheses Success")
 }
+
+func Test_LongestValidParenthesesFour(t *testing.T) {
+	source := ")()())"
+	target := longestValidParentheses(source)
+	expect := 4
+
+	if expect != target {
+		t.Error("Translate Test_LongestValidParenthesesFour Fail", target)
+	}
+	t.Log("Translate Test_LongestValidParenthesesFour Success")
+}
+
+func Test_LongestValidParenthesesCase(t *testing.T) {
+	source := "()(())))()(()("
+	target := longestValidParentheses(source)
+	expect := 6
+
+	if expect != target {
+		t.Error("Translate Test_LongestValidParenthesesCase Fail", target)
+	}
+	t.Log("Translate Test_LongestValidParenthesesCase Success")
+}
+
+func Test_LongestValidParenthesesMore(t *testing.T) {
+	source := "()(())))()(()())"
+	target := longestValidParentheses(source)
+	expect := 8
+
+	if expect != target {
+		t.Error("Translate Test_LongestValidParenthesesMore Fail", target)
+	}
+	t.Log("Translate Test_LongestValidParenthesesMore Success")
+}
