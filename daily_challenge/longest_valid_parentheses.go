5dcb7028bf3204077a38065428c96d15c50af7a0
liuwill <liuwei_will@qq.com>
2018-09-07 14:34:57 +0800

    change method and detail missing

--- daily_challenge/longest_valid_parentheses.go
+++ daily_challenge/longest_valid_parentheses.go
@@ -1,31 +1,43 @@
 package daily_challenge
 
+import "fmt"
+
 func longestValidParentheses(s string) int {
+	println(s, "===========")
 	max := 0
 	left := '('
 	right := ')'
 
-	current := 0
 	match := 0
-
-	for _, v := range s {
+	// listPos := 0
+	matchList := make([]int, len(s))
+	last := []int{}
+	for i, v := range s {
+		fmt.Printf("matchList=%v last=%v", matchList, last)
+		println(" match=", match, " i=", i, " v=", string(v))
 		if v == left {
+			matchList[match] = i
 			match++
-		}
-
-		if v == right {
-			if match > 0 {
-				match--
-				current += 2
-			} else {
-				current = 0
+		} else if v == right {
+			if match <= 0 {
 				match = 0
+				continue
+			}
+
+			match--
+			current := i - matchList[match] + 1
+			if len(last) > 0 && matchList[match]-1 == last[1] {
+				last = []int{last[0], i}
+				current = i - last[0] + 1
+			} else {
+				last = []int{matchList[match], i}
+			}
+
+			if current > max {
+				max = current
 			}
 		}
-
-		if current > max {
-			max = current
-		}
 	}
+
 	return max
 }
--- daily_challenge/longest_valid_parentheses_test.go
+++ daily_challenge/longest_valid_parentheses_test.go
@@ -13,6 +13,39 @@ func Test_LongestValidParentheses(t *testing.T) {
 	t.Log("Translate Test_LongestValidParentheses Success")
 }
 
+func Test_LongestValidParenthesesEmpty(t *testing.T) {
+	source := ""
+	target := longestValidParentheses(source)
+	expect := 0
+
+	if expect != target {
+		t.Error("Translate Test_LongestValidParenthesesEmpty Fail", target)
+	}
+	t.Log("Translate Test_LongestValidParenthesesEmpty Success")
+}
+
+func Test_LongestValidParenthesesError(t *testing.T) {
+	source := "()(()"
+	target := longestValidParentheses(source)
+	expect := 2
+
+	if expect != target {
+		t.Error("Translate Test_LongestValidParenthesesError Fail", target)
+	}
+	t.Log("Translate Test_LongestValidParenthesesError Success")
+}
+
+func Test_LongestValidParenthesesLong(t *testing.T) {
+	source := "()((())"
+	target := longestValidParentheses(source)
+	expect := 4
+
+	if expect != target {
+		t.Error("Translate Test_LongestValidParenthesesLong Fail", target)
+	}
+	t.Log("Translate Test_LongestValidParenthesesLong Success")
+}
+
 func Test_LongestValidParenthesesFour(t *testing.T) {
 	source := ")()())"
 	target := longestValidParentheses(source)
