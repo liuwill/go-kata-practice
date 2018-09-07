337b6ac6585a96f76499e6b6ea41128ad550ba15
liuwill <liuwei_will@qq.com>
2018-09-07 14:52:09 +0800

    consider merge and pass

--- daily_challenge/longest_valid_parentheses.go
+++ daily_challenge/longest_valid_parentheses.go
@@ -1,41 +1,49 @@
 package daily_challenge
 
-import "fmt"
-
 func longestValidParentheses(s string) int {
-	println(s, "===========")
+	// println(s, "===========")
 	max := 0
 	left := '('
-	right := ')'
+	// right := ')'
 
 	match := 0
-	// listPos := 0
+	validPos := 0
 	matchList := make([]int, len(s))
-	last := []int{}
+	validList := make([][]int, len(s)/2+1)
 	for i, v := range s {
-		fmt.Printf("matchList=%v last=%v", matchList, last)
-		println(" match=", match, " i=", i, " v=", string(v))
+		// fmt.Printf("matchList=%v validList=%v", matchList, validList)
+		// println(" match=", match, " i=", i, " v=", string(v))
 		if v == left {
 			matchList[match] = i
 			match++
-		} else if v == right {
-			if match <= 0 {
-				match = 0
-				continue
-			}
+			continue
+		}
 
-			match--
-			current := i - matchList[match] + 1
-			if len(last) > 0 && matchList[match]-1 == last[1] {
-				last = []int{last[0], i}
+		if match <= 0 {
+			match = 0
+			continue
+		}
+
+		match--
+		current := i - matchList[match] + 1
+		isMerge := false
+		for j := 0; j < validPos; j++ {
+			last := validList[j]
+			if last[1]+1 == matchList[match] {
 				current = i - last[0] + 1
-			} else {
-				last = []int{matchList[match], i}
+				validList[j] = []int{last[0], i}
+				validPos = j + 1
+				isMerge = true
+				break
 			}
+		}
+		if !isMerge {
+			validList[validPos] = []int{matchList[match], i}
+			validPos++
+		}
 
-			if current > max {
-				max = current
-			}
+		if current > max {
+			max = current
 		}
 	}
 
