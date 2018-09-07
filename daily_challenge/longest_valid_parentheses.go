77081ceff38ffc0a9572f3edd5ba46034b6b6250
liuwill <liuwei_will@qq.com>
2018-09-07 10:14:24 +0800

    solve match valid parentheses test

--- daily_challenge/longest_valid_parentheses.go
+++ daily_challenge/longest_valid_parentheses.go
@@ -1,5 +1,31 @@
 package daily_challenge
 
 func longestValidParentheses(s string) int {
-	return 0
+	max := 0
+	left := '('
+	right := ')'
+
+	current := 0
+	match := 0
+
+	for _, v := range s {
+		if v == left {
+			match++
+		}
+
+		if v == right {
+			if match > 0 {
+				match--
+				current += 2
+			} else {
+				current = 0
+				match = 0
+			}
+		}
+
+		if current > max {
+			max = current
+		}
+	}
+	return max
 }
