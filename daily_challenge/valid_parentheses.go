ac281950467c52173c4fa5b01ba5878de14cfeef
liuwill <liuwei_will@qq.com>
2018-09-09 14:53:09 +0800

    finish coding

--- daily_challenge/valid_parentheses.go
+++ daily_challenge/valid_parentheses.go
@@ -1,5 +1,40 @@
 package daily_challenge
 
-func isValid(s string) bool {
+func isLeftParenthese(letter rune) bool {
+	if letter == '{' || letter == '[' || letter == '(' {
+		return true
+	}
+
 	return false
+}
+
+func isValid(s string) bool {
+	pos := 0
+	temp := make([]rune, len(s))
+	parentheseMap := map[rune]rune{
+		')': '(',
+		'}': '{',
+		']': '[',
+	}
+
+	answer := true
+	for _, v := range s {
+		if isLeftParenthese(v) {
+			temp[pos] = v
+			pos++
+		} else {
+			left := parentheseMap[v]
+			if pos < 1 || left != temp[pos-1] {
+				answer = false
+				break
+			}
+
+			pos--
+		}
+	}
+
+	if pos > 0 {
+		answer = false
+	}
+	return answer
 }
