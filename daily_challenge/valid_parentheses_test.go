b94f61ddddf19b4006d88baea3c1c181bd6079dd
liuwill <liuwei_will@qq.com>
2018-09-09 14:55:25 +0800

    test by case

--- daily_challenge/valid_parentheses_test.go
+++ daily_challenge/valid_parentheses_test.go
@@ -12,3 +12,47 @@ func Test_IsValid(t *testing.T) {
 	}
 	t.Log("Translate Test_IsValid Success")
 }
+
+func Test_IsValidFull(t *testing.T) {
+	source := "()[]{}"
+	target := isValid(source)
+	expect := true
+
+	if expect != target {
+		t.Error("Translate Test_IsValidFull Fail", target)
+	}
+	t.Log("Translate Test_IsValidFull Success")
+}
+
+func Test_IsValidFalse(t *testing.T) {
+	source := "(]"
+	target := isValid(source)
+	expect := false
+
+	if expect != target {
+		t.Error("Translate Test_IsValidFalse Fail", target)
+	}
+	t.Log("Translate Test_IsValidFalse Success")
+}
+
+func Test_IsValidFalseMore(t *testing.T) {
+	source := "([)]"
+	target := isValid(source)
+	expect := false
+
+	if expect != target {
+		t.Error("Translate Test_IsValidFalseMore Fail", target)
+	}
+	t.Log("Translate Test_IsValidFalseMore Success")
+}
+
+func Test_IsValidTrue(t *testing.T) {
+	source := "{[]}"
+	target := isValid(source)
+	expect := true
+
+	if expect != target {
+		t.Error("Translate Test_IsValidTrue Fail", target)
+	}
+	t.Log("Translate Test_IsValidTrue Success")
+}
