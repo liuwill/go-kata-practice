235a376f1b949a914893f9cce68ce3609fe822f7
liuwill <liuwei_will@qq.com>
2018-09-11 16:50:40 +0800

    test monotonic array and coverage 100%

--- weekly_contest_100/monotonic_array_test.go
+++ weekly_contest_100/monotonic_array_test.go
@@ -12,3 +12,36 @@ func Test_IsMonotonic(t *testing.T) {
 	}
 	t.Log("Translate Test_IsMonotonic Success")
 }
+
+func Test_IsMonotonicSmall(t *testing.T) {
+	source := []int{1}
+	target := isMonotonic(source)
+	expect := true
+
+	if expect != target {
+		t.Error("Translate Test_IsMonotonicSmall Fail", target)
+	}
+	t.Log("Translate Test_IsMonotonicSmall Success")
+}
+
+func Test_IsMonotonicDecrease(t *testing.T) {
+	source := []int{6, 5, 4, 4}
+	target := isMonotonic(source)
+	expect := true
+
+	if expect != target {
+		t.Error("Translate Test_IsMonotonicDecrease Fail", target)
+	}
+	t.Log("Translate Test_IsMonotonicDecrease Success")
+}
+
+func Test_IsMonotonicFail(t *testing.T) {
+	source := []int{1, 3, 2}
+	target := isMonotonic(source)
+	expect := false
+
+	if expect != target {
+		t.Error("Translate Test_IsMonotonicFail Fail", target)
+	}
+	t.Log("Translate Test_IsMonotonicFail Success")
+}
