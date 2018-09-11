f39b6b923e2e6a42ed797d417918ca8dc03aec7a
liuwill <liuwei_will@qq.com>
2018-09-11 10:13:04 +0800

    finish code with match and left shift

--- daily_challenge/score_parentheses.go
+++ daily_challenge/score_parentheses.go
@@ -1,5 +1,21 @@
 package daily_challenge
 
 func scoreOfParentheses(S string) int {
-	return len(S) / 2
+	left := '('
+
+	count := 0
+	result := 0
+	for i, v := range S {
+		if v == left {
+			count++
+			continue
+		}
+
+		if int(S[i-1]) == int(left) {
+			result += 1 << uint16(count-1)
+		}
+
+		count--
+	}
+	return result
 }
