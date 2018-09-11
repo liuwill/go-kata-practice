9bd846329a354e34e961d0e705d2c2cd973913ea
liuwill <liuwei_will@qq.com>
2018-09-11 10:31:23 +0800

    fully test socre of parentheses

--- daily_challenge/score_parentheses_test.go
+++ daily_challenge/score_parentheses_test.go
@@ -13,6 +13,17 @@ func Test_ScoreOfParentheses(t *testing.T) {
 	t.Log("Translate Test_ScoreOfParentheses Success")
 }
 
+func Test_ScoreOfParenthesesEmpty(t *testing.T) {
+	source := ""
+	target := scoreOfParentheses(source)
+	expect := 0
+
+	if expect != target {
+		t.Error("Translate Test_ScoreOfParenthesesEmpty Fail", target)
+	}
+	t.Log("Translate Test_ScoreOfParenthesesEmpty Success")
+}
+
 func Test_ScoreOfParenthesesTwo(t *testing.T) {
 	source := "()()"
 	target := scoreOfParentheses(source)
@@ -22,4 +33,26 @@ func Test_ScoreOfParenthesesTwo(t *testing.T) {
 		t.Error("Translate Test_ScoreOfParenthesesTwo Fail", target)
 	}
 	t.Log("Translate Test_ScoreOfParenthesesTwo Success")
+}
+
+func Test_ScoreOfParenthesesLayer(t *testing.T) {
+	source := "(())"
+	target := scoreOfParentheses(source)
+	expect := 2
+
+	if expect != target {
+		t.Error("Translate Test_ScoreOfParenthesesLayer Fail", target)
+	}
+	t.Log("Translate Test_ScoreOfParenthesesLayer Success")
+}
+
+func Test_ScoreOfParenthesesDeep(t *testing.T) {
+	source := "(()(()))"
+	target := scoreOfParentheses(source)
+	expect := 6
+
+	if expect != target {
+		t.Error("Translate Test_ScoreOfParenthesesDeep Fail", target)
+	}
+	t.Log("Translate Test_ScoreOfParenthesesDeep Success")
 }
