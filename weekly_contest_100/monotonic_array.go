ecebba3c950e5f3854aa6f24b1ed0a1b82e85526
liuwill <liuwei_will@qq.com>
2018-09-11 16:48:17 +0800

    coding monotonic array finish

--- weekly_contest_100/monotonic_array.go
+++ weekly_contest_100/monotonic_array.go
@@ -1,5 +1,20 @@
 package weekly_contest_100
 
 func isMonotonic(A []int) bool {
-	return true
+	increasing := true
+	decreasing := true
+	for i := 1; i < len(A); i++ {
+		if A[i] > A[i-1] {
+			decreasing = false
+		}
+
+		if A[i] < A[i-1] {
+			increasing = false
+		}
+
+		if !increasing && !decreasing {
+			break
+		}
+	}
+	return increasing || decreasing
 }
