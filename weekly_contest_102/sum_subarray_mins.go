03045269458f5a6c0773dba103a8f53f4393f8b0
liuwill <liuwei_will@qq.com>
2018-09-29 14:22:16 +0800

    init sum subarray mins

---
+++ weekly_contest_102/sum_subarray_mins.go
@@ -0,0 +1,10 @@
+package weekly_contest_102
+
+import "sort"
+
+func sumSubarrayMins(A []int) int {
+	sum := 0
+	sort.Ints(A)
+
+	return sum
+}
---
+++ weekly_contest_102/sum_subarray_mins_test.go
@@ -0,0 +1,24 @@
+package weekly_contest_102
+
+import (
+	"testing"
+)
+
+var (
+	subSources = [][]int{
+		{3, 1, 2, 4},
+	}
+	subExpects = []int{17}
+)
+
+func Test_SumSubarrayMins(t *testing.T) {
+	for i, source := range subSources {
+		target := sumSubarrayMins(source)
+		expect := subExpects[i]
+
+		if expect != target {
+			t.Error("Run Test_SumSubarrayMins Fail", source, target)
+		}
+	}
+	t.Log("Run Test_SumSubarrayMins Success")
+}
