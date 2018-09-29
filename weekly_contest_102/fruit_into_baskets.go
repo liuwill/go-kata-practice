49fb29dcf306b9504488219bc9cc9479e505b122
liuwill <liuwei_will@qq.com>
2018-09-29 10:15:37 +0800

    finish coding fruit

--- weekly_contest_102/fruit_into_baskets.go
+++ weekly_contest_102/fruit_into_baskets.go
@@ -1,5 +1,33 @@
 package weekly_contest_102
 
 func totalFruit(tree []int) int {
-	return len(tree)
+	max := 0
+	length := len(tree)
+
+	for i, cursor := range tree {
+		left := length - i
+		if left < max {
+			break
+		}
+
+		current := 1
+		second := 0
+		for _, next := range tree[i+1:] {
+			if next == cursor {
+				current++
+			} else if second != 0 && next == second {
+				current++
+			} else if second == 0 {
+				current++
+				second = next
+			} else {
+				break
+			}
+		}
+
+		if max < current {
+			max = current
+		}
+	}
+	return max
 }
