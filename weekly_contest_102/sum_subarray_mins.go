1816b0d4e08b7077da66302a871d931c337937bd
liuwill <liuwei_will@qq.com>
2018-09-29 14:39:01 +0800

    a solution basic frame

--- weekly_contest_102/sum_subarray_mins.go
+++ weekly_contest_102/sum_subarray_mins.go
@@ -1,10 +1,25 @@
 package weekly_contest_102
 
-import "sort"
+import (
+	"math"
+	"sort"
+)
 
 func sumSubarrayMins(A []int) int {
+	maxSum := int(math.Pow10(9)) + 7
 	sum := 0
 	sort.Ints(A)
 
-	return sum
+	count := 0
+	times := 0
+	for i := len(A) - 1; i >= 0; i-- {
+		val := A[i]
+
+		times += int(math.Pow(2, float64(count)))
+		sum += val * (times) % maxSum
+		println(val, i)
+		count++
+	}
+
+	return sum % maxSum
 }
