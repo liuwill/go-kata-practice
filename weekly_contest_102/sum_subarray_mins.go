f4a9fd2ea12faf7ff5ac771a8cdafca865a910b5
liuwill <liuwei_will@qq.com>
2018-09-29 15:48:40 +0800

    develop a simple direct way

--- weekly_contest_102/sum_subarray_mins.go
+++ weekly_contest_102/sum_subarray_mins.go
@@ -8,17 +8,32 @@ func sumSubarrayMins(A []int) int {
 	maxSum := int(math.Pow10(9)) + 7
 	sum := 0
 
-	for i, val := range A {
-		for d := 1; d < len(A)-i+1; d++ {
-			min := val
-			// fmt.Printf("%v\n", A[i:i+d])
-			for _, cursor := range A[i : i+d] {
-				if cursor < min {
-					min = cursor
-				}
+	for _, val := range A {
+		sum += val
+	}
+
+	for len(A) > 1 {
+		min := 30001
+		pos := 0
+		// fmt.Printf("++++ %v\n", A)
+		for i, val := range A {
+			if val < min {
+				min = val
+				pos = i
+
+				sum += val * i
+				// println("==>", val, i, val*i)
+			} else {
+				sum += min * (i - pos)
+				// println("=====>", val, i, i-pos, val*(i-pos))
 			}
+			// println(sum, "===", i, val, pos, min)
+		}
+
+		if pos > 0 {
 			sum += min
 		}
+		A = A[pos+1:]
 	}
 
 	return sum % maxSum
