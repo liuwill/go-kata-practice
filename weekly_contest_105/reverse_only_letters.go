fccfd08bc725058912780c488567ba4328040b53
liuwill <liuwei_will@qq.com>
2018-10-16 13:48:49 +0800

    first try

--- weekly_contest_105/reverse_only_letters.go
+++ weekly_contest_105/reverse_only_letters.go
@@ -1,5 +1,30 @@
 package weekly_contest_105
 
-func reverseOnlyLetters(S string) string {
+func isLetter(letter byte) bool {
+	if letter >= 97 && letter <= 122 {
+		return true
+	} else if letter >= 65 && letter <= 90 {
+		return true
+	}
 
+	return false
+}
+
+func reverseOnlyLetters(S string) string {
+	result := ""
+	j := len(S) - 1
+	for i := 0; i < len(S) && j >= 0; {
+		if !isLetter(S[i]) {
+			result += string(S[i])
+			i++
+		} else if !isLetter(S[j]) {
+			j--
+		} else {
+			result += string(S[j])
+			i++
+			j--
+		}
+	}
+
+	return result
 }
