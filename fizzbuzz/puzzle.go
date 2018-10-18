902db4c3d79ecccf9a12750e2926773b857879c1
liuwill <liuwei_will@qq.com>
2018-10-18 19:45:44 +0800

    make fizz buzz speed up

--- fizzbuzz/puzzle.go
+++ fizzbuzz/puzzle.go
@@ -20,7 +20,29 @@ func fizzBuzz(n int) []string {
 		}
 
 		result[i-1] = current
-		println(current, i)
+	}
+	return result
+}
+
+func fizzBuzzForSpeed(n int) []string {
+	result := make([]string, n)
+	for i := 1; i <= n; i++ {
+		current := ""
+
+		isFizz := i%3 == 0
+		isBuzz := i%5 == 0
+		if isFizz && isBuzz {
+			current = "FizzBuzz"
+		} else if isFizz {
+			current = "Fizz"
+		} else if isBuzz {
+			current = "Buzz"
+		} else {
+			current = strconv.Itoa(i)
+		}
+
+		// println(current, i)
+		result[i-1] = current
 	}
 	return result
 }
--- fizzbuzz/puzzle_test.go
+++ fizzbuzz/puzzle_test.go
@@ -14,3 +14,14 @@ func Test_FizzBuzz(t *testing.T) {
 
 	t.Log("Run Test_FizzBuzz Success")
 }
+
+func Test_FizzBuzzForSpeed(t *testing.T) {
+	expect := 15
+	target := fizzBuzzForSpeed(expect)
+
+	if expect != len(target) {
+		t.Error("Run fizzBuzzForSpeed Fail", target)
+	}
+
+	t.Log("Run fizzBuzzForSpeed Success")
+}
