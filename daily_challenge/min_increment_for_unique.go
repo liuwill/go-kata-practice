ab187cd7fc842910bb1a734e05428d82f2ca71e8
liuwill <liuwei_will@qq.com>
2018-12-04 19:46:02 +0800

    finish minIncrementForUnique

--- daily_challenge/min_increment_for_unique.go
+++ daily_challenge/min_increment_for_unique.go
@@ -1,6 +1,21 @@
 package daily_challenge
 
-func minIncrementForUnique(A []int) int {
+import (
+	"sort"
+)
 
-	return len(A)
+func minIncrementForUnique(A []int) int {
+	sort.Ints(A)
+	count := 0
+	for i, v := range A {
+		if i == 0 {
+			continue
+		}
+
+		if v <= A[i-1] {
+			count += A[i-1] - v + 1
+			A[i] += A[i-1] - v + 1
+		}
+	}
+	return count
 }
--- daily_challenge/min_increment_for_unique_test.go
+++ daily_challenge/min_increment_for_unique_test.go
@@ -4,9 +4,21 @@ import (
 	"testing"
 )
 
-func Test_MinIncrementForUnique(t *testing.T) {
+func Test_MinIncrementForUniqueEasy(t *testing.T) {
 	source := []int{1, 2, 2}
 	expect := 1
+
+	target := minIncrementForUnique(source)
+
+	if target != expect {
+		t.Error("Translate Test_MinIncrementForUniqueEasy Fail", target, expect)
+	}
+	t.Log("Translate Test_MinIncrementForUniqueEasy Success")
+}
+
+func Test_MinIncrementForUnique(t *testing.T) {
+	source := []int{3, 2, 1, 2, 1, 7}
+	expect := 6
 
 	target := minIncrementForUnique(source)
 
