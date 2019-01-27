64bf5e3acd4e398231565deae608b8df1d66b4f7
liuwill <liuwei_will@qq.com>
2019-01-27 17:14:08 +0800

    code finish

--- weekly_contest/string_without_ab.go
+++ weekly_contest/string_without_ab.go
@@ -1,10 +1,39 @@
 package weekly_content
 
 /**
- * weekly-contest-120
- * PUZZLE: squares-of-a-sorted-array
+ * weekly-contest-121
+ * PUZZLE: String Without AAA or BBB
  */
 
+const MAX_DUPLICATE_SIZE = 2
+
 func strWithout3a3b(A int, B int) string {
-	return ""
+	target := ""
+
+	letterDict := map[rune]int{
+		'a': A,
+		'b': B,
+	}
+
+	reverseDict := map[rune]rune{
+		'a': 'b',
+		'b': 'a',
+	}
+
+	current := 'a'
+	size := 0
+
+	max := A + B
+
+	for len(target) < max {
+		if letterDict[current] < 0 || size >= MAX_DUPLICATE_SIZE {
+			current = reverseDict[current]
+			size = 0
+		} else {
+			size++
+			target += string(current)
+		}
+	}
+
+	return target
 }
