3c6d788c7e5c5d26c626321b5d0e76ed548bd124
liuwill <liuwei_will@qq.com>
2019-02-01 17:28:45 +0800

    finish buddy strings

--- README.md
+++ README.md
@@ -58,6 +58,7 @@ JUST make some practice, In this repository questions came from leetcode
 -   [Score Of Parentheses](./docs/daily_challenge/score_of_parentheses.md)
 -   [Minimum Increment to Make Array Unique](./docs/daily_challenge/min_increment_for_unique.md)
 -   [Perfect Squares](./docs/daily_challenge/perfect_squares.md)
+-   [Buddy Strings](./docs/daily_challenge/buddy_strings.md)
 
 
 #### Weekly Contest
--- daily_challenge/buddy_strings.go
+++ daily_challenge/buddy_strings.go
@@ -1,9 +1,23 @@
 package daily_challenge
 
+import "strings"
+
 /**
  * daily-challenge-859
  * PUZZLE: Buddy Strings
  */
 func buddyStrings(A string, B string) bool {
+	if len(A) != len(B) {
+		return false
+	}
+
+	for i, _ := range A {
+		for j := i + 1; j < len(A); j++ {
+			newA := strings.Join([]string{A[:i], string(A[j]), A[i+1 : j], string(A[i]), A[j+1:]}, "")
+			if newA == B {
+				return true
+			}
+		}
+	}
 	return false
 }
