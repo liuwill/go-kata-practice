52fb740e22f6e4ed89ff2a2c29a55825c3d9e5ad
liuwill <liuwei_will@qq.com>
2019-02-13 11:27:35 +0800

    partition lables work pass basic test

--- daily_challenge/partition_labels.go
+++ daily_challenge/partition_labels.go
@@ -1,5 +1,23 @@
 package daily_challenge
 
 func partitionLabels(S string) []int {
-	return nil
+	target := []int{}
+	start := 0
+	end := 0
+	for i := 0; i < len(S); i++ {
+		current := S[i]
+		if i > end || i == len(S)-1 {
+			// println(i, start, end)
+			target = append(target, end-start+1)
+			start = i
+		}
+
+		for j := len(S) - 1; j > i; j-- {
+			if current == S[j] {
+				end = j
+				break
+			}
+		}
+	}
+	return target
 }
