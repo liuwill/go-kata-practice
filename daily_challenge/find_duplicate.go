2fc4498aa8378e65497d7b0a2fb97fb08ee10b3b
liuwill <liuwei_will@qq.com>
2019-02-14 10:38:05 +0800

    find duplicate number with the simplest way

--- daily_challenge/find_duplicate.go
+++ daily_challenge/find_duplicate.go
@@ -5,5 +5,21 @@ package daily_challenge
  * PUZZLE: Find the Duplicate Number
  */
 func findDuplicate(nums []int) int {
-	return len(nums)
+	target := 0
+	for i, val := range nums {
+		isFind := false
+		j := i + 1
+		for ; j < len(nums); j++ {
+			if val == nums[j] {
+				isFind = true
+				break
+			}
+		}
+
+		if isFind {
+			target = val
+			break
+		}
+	}
+	return target
 }
