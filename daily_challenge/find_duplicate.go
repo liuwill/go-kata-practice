89f1f6023c1e9804f1da5d61b240fbc9ded69c8d
liuwill <liuwei_will@qq.com>
2019-02-14 10:34:25 +0800

    init Find the Duplicate Number

---
+++ daily_challenge/find_duplicate.go
@@ -0,0 +1,9 @@
+package daily_challenge
+
+/**
+ * daily-challenge-287
+ * PUZZLE: Find the Duplicate Number
+ */
+func findDuplicate(nums []int) int {
+	return len(nums)
+}
---
+++ daily_challenge/find_duplicate_test.go
@@ -0,0 +1,25 @@
+package daily_challenge
+
+import "testing"
+
+func Test_FindDuplicate(t *testing.T) {
+	sourceCase := [][]int{
+		{1, 3, 4, 2, 2},
+		{3, 1, 3, 4, 2},
+	}
+	expectCase := []int{
+		2,
+		3,
+	}
+
+	for i, source := range sourceCase {
+		expect := expectCase[i]
+
+		target := findDuplicate(source)
+		if target != expect {
+			t.Error("Translate Test_FindDuplicate Fail", expect, target)
+		}
+	}
+
+	t.Log("Translate Test_FindDuplicate Success")
+}
