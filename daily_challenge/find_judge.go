8f337ec00b038d3ae2dd5e94008d18b3a5614c16
liuwill <liuwei_will@qq.com>
2019-03-05 11:41:54 +0800

    simply finish find judge

--- daily_challenge/find_judge.go
+++ daily_challenge/find_judge.go
@@ -1,5 +1,23 @@
 package daily_challenge
 
 func findJudge(N int, trust [][]int) int {
-	return N
+	trustMarks := make([]int, N)
+	trustedMarks := make([]int, N)
+
+	for _, relation := range trust {
+		trustMarks[relation[0]-1] = 1
+		trustedMarks[relation[1]-1] += 1
+	}
+
+	target := -1
+	for i := 1; i <= N; i++ {
+		pos := i - 1
+		if trustMarks[pos] == 0 && trustedMarks[pos] == N-1 {
+			if target > 0 {
+				return -1
+			}
+			target = i
+		}
+	}
+	return target
 }
