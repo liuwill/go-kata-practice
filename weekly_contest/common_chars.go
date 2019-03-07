5342eb9470dfd514178631dde453db58c5fe221f
liuwill <liuwei_will@qq.com>
2019-03-07 09:40:44 +0800

    finish commonChars

--- weekly_contest/common_chars.go
+++ weekly_contest/common_chars.go
@@ -1,5 +1,23 @@
 package weekly_contest
 
 func commonChars(A []string) []string {
-	return []string{}
+	countList := make([]int, 26)
+	FIRST_LETTER := 'a'
+	for _, list := range A {
+		for _, letter := range list {
+			pos := letter - FIRST_LETTER
+			countList[pos] += 1
+		}
+	}
+
+	target := []string{}
+	for pos, count := range countList {
+		letter := rune(pos) + FIRST_LETTER
+
+		if count == len(A) {
+			target = append(target, string(letter))
+		}
+	}
+
+	return target
 }
