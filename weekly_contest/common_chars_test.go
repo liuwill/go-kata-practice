57aa7ecb95624bbd8209d3254fd4ab6676b09ba8
liuwill <liuwei_will@qq.com>
2019-03-07 09:47:03 +0800

    tet comman chars

---
+++ weekly_contest/common_chars_test.go
@@ -0,0 +1,43 @@
+package weekly_contest
+
+import (
+	"sort"
+	"testing"
+)
+
+func compareListWithoutOrder(source []string, target []string) bool {
+	if len(source) != len(target) {
+		return false
+	}
+	sort.Strings(source)
+	sort.Strings(target)
+
+	for i, letter := range source {
+		if target[i] != letter {
+			return false
+		}
+	}
+	return true
+}
+
+func Test_CommonChars(t *testing.T) {
+	sourceCase := [][]string{
+		{"bella", "label", "roller"},
+		{"cool", "lock", "cook"},
+	}
+	expectList := [][]string{
+		{"e", "l", "l"},
+		{"c", "o"},
+	}
+
+	for i, source := range sourceCase {
+		expect := expectList[i]
+
+		target := commonChars(source)
+		if !compareListWithoutOrder(target, expect) {
+			t.Error("Run Test_CommonChars Fail", expect, target)
+		}
+	}
+
+	t.Log("Run Test_CommonChars Success")
+}
