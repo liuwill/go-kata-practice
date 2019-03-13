bdf03dbca629f649dee014332ac45f7c2fb87a29
liuwill <liuwei_will@qq.com>
2019-03-13 09:55:31 +0800

    coding done

--- weekly_contest/powerful_integers.go
+++ weekly_contest/powerful_integers.go
@@ -1,5 +1,28 @@
 package weekly_contest
 
+import "math"
+
+func powerInt(num int, power int) int {
+	result := math.Pow(float64(num), float64(power))
+	return int(result)
+}
+
 func powerfulIntegers(x int, y int, bound int) []int {
-	return nil
+	source := make([]int, bound+1)
+	for i := 0; powerInt(x, i)+1 <= bound; i++ {
+		for j := 0; powerInt(y, j)+1 <= bound; j++ {
+			val := powerInt(x, i) + powerInt(y, j)
+			if val <= bound {
+				source[val]++
+			}
+		}
+	}
+
+	target := []int{}
+	for _, val := range source {
+		if val > 0 {
+			target = append(target, val)
+		}
+	}
+	return target
 }
