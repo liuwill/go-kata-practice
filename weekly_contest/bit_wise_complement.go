f04e81575c628c2497a3cadb1a22461c05b821ef
liuwill <liuwei_will@qq.com>
2019-03-19 15:20:37 +0800

    improve Complement of Base 10 Integer

--- weekly_contest/bit_wise_complement.go
+++ weekly_contest/bit_wise_complement.go
@@ -7,24 +7,25 @@ import "math"
  * PUZZLE: Complement of Base 10 Integer
  */
 func bitwiseComplement(N int) int {
-	target := ""
-	source := ""
+	// target := ""
+	// source := ""
 
 	copy := N
 	if copy == 0 {
 		return 1
 	}
+
+	i := 0
+	result := 0
 	for copy > 0 {
 		bit := copy % 2
-		source = string(bit) + source
-		target += string(bit ^ 1)
+		// source = string(bit) + source
+		// target += string(bit ^ 1)
+		reverse := bit ^ 1
+		result += int(reverse) * int(math.Pow(float64(2), float64(i)))
 
 		copy = copy / 2
-	}
-
-	result := 0
-	for i, bit := range target {
-		result += int(bit) * int(math.Pow(float64(2), float64(i)))
+		i++
 	}
 
 	return result
