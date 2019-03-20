ea09c82527bf0222fb42d12f10931ae52e044ca0
liuwill <liuwei_will@qq.com>
2019-03-20 11:30:36 +0800

    bulb swither works

--- daily_challenge/bulb_switcher.go
+++ daily_challenge/bulb_switcher.go
@@ -1,5 +1,24 @@
 package daily_challenge
 
+/**
+ * daily-challenge-319
+ * PUZZLE: Bulb Switcher
+ */
 func bulbSwitch(n int) int {
-	return n
+	list := make([]int, n)
+
+	for i := 1; i <= n; i++ {
+		for pos := 1; pos < n; pos += i + 1 {
+			list[pos] = list[pos] ^ 1
+		}
+	}
+
+	target := 0
+	for _, val := range list {
+		if val == 1 {
+			target++
+		}
+	}
+	// fmt.Printf("BULB: %v \n", list)
+	return target
 }
