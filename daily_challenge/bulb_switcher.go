c2efe7c95cc5db0f81a6faae77e3e5fc106339fd
liuwill <liuwei_will@qq.com>
2019-03-20 10:51:18 +0800

    ini Bulb Switcher

---
+++ daily_challenge/bulb_switcher.go
@@ -0,0 +1,5 @@
+package daily_challenge
+
+func bulbSwitch(n int) int {
+	return n
+}
---
+++ daily_challenge/bulb_switcher_test.go
@@ -0,0 +1,25 @@
+package daily_challenge
+
+import (
+	"testing"
+)
+
+func Test_BulbSwitch(t *testing.T) {
+	sourceCase := []int{
+		3,
+	}
+	expectList := []int{
+		1,
+	}
+
+	for i, source := range sourceCase {
+		expect := expectList[i]
+
+		target := bulbSwitch(source)
+		if target != expect {
+			t.Error("Run Test_BulbSwitch Fail", source, expect, target)
+		}
+	}
+
+	t.Log("Run Test_BulbSwitch Success")
+}
