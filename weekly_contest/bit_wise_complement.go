abe39fa3b08d650b5ba17145af285669fc363b6f
liuwill <liuwei_will@qq.com>
2019-03-20 09:52:21 +0800

    improve bitwiseComplement performance

--- weekly_contest/bit_wise_complement.go
+++ weekly_contest/bit_wise_complement.go
@@ -1,7 +1,5 @@
 package weekly_contest
 
-import "math"
-
 /**
  * daily-challenge-1012
  * PUZZLE: Complement of Base 10 Integer
@@ -15,17 +13,17 @@ func bitwiseComplement(N int) int {
 		return 1
 	}
 
-	i := 0
+	i := 1
 	result := 0
 	for copy > 0 {
 		bit := copy % 2
 		// source = string(bit) + source
 		// target += string(bit ^ 1)
 		reverse := bit ^ 1
-		result += int(reverse) * int(math.Pow(float64(2), float64(i)))
+		result += int(reverse) * i
 
 		copy = copy / 2
-		i++
+		i = i * 2
 	}
 
 	return result
