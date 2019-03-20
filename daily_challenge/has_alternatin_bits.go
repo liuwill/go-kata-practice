64f5e4123909daa21dc03df19aab0784184e0eb6
liuwill <liuwei_will@qq.com>
2019-03-20 10:16:24 +0800

    just coding has alternatin bits done

--- daily_challenge/has_alternatin_bits.go
+++ daily_challenge/has_alternatin_bits.go
@@ -1,5 +1,24 @@
 package daily_challenge
 
 func hasAlternatingBits(n int) bool {
-	return false
+	result := true
+	if n < 2 {
+		return result
+	}
+
+	bit := n % 2
+	n = n / 2
+
+	current := bit
+	for n > 0 {
+		bit := n % 2
+		if bit == current {
+			result = false
+			break
+		}
+
+		current = bit
+	}
+
+	return result
 }
---
+++ daily_challenge/has_alternatin_bits_test.go
@@ -0,0 +1,31 @@
+package daily_challenge
+
+import (
+	"testing"
+)
+
+func Test_HasAlternatingBits(t *testing.T) {
+	sourceCase := []int{
+		5,
+		7,
+		11,
+		10,
+	}
+	expectList := []bool{
+		true,
+		false,
+		false,
+		true,
+	}
+
+	for i, source := range sourceCase {
+		expect := expectList[i]
+
+		target := hasAlternatingBits(source)
+		if target != expect {
+			t.Error("Run Test_HasAlternatingBits Fail", source, expect, target)
+		}
+	}
+
+	t.Log("Run Test_HasAlternatingBits Success")
+}
