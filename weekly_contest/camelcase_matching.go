c33ed9134b27693c187919b1ecfddaebeb00f17e
liuwill <liuwei_will@qq.com>
2019-04-14 14:43:42 +0800

    start Camelcase Matching

---
+++ weekly_contest/camelcase_matching.go
@@ -0,0 +1,10 @@
+package weekly_contest
+
+/**
+ * daily-challenge-1023
+ * PUZZLE: Camelcase Matching
+ */
+func camelMatch(queries []string, pattern string) []bool {
+	target := make([]bool, len(queries))
+	return target
+}
