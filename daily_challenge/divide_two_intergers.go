d8c615eab8521975e50224e44b9fb28b34973e38
liuwill <liuwei_will@qq.com>
2019-04-18 14:29:46 +0800

    finish divide two interger

--- daily_challenge/divide_two_intergers.go
+++ daily_challenge/divide_two_intergers.go
@@ -1,5 +1,35 @@
 package daily_challenge
 
+import "math"
+
 func divide(dividend int, divisor int) int {
-	return dividend / divisor
+	result := 0
+	max := int(math.Pow(2, 31) - 1)
+	min := int(math.Pow(2, 31)) * -1
+
+	mark := false
+	if dividend < 0 {
+		mark = !mark
+		dividend *= -1
+	}
+	if divisor < 0 {
+		mark = !mark
+		divisor *= -1
+	}
+
+	for dividend >= divisor {
+		dividend -= divisor
+
+		if mark {
+			result--
+		} else {
+			result++
+		}
+
+		if result >= max || result <= min {
+			break
+		}
+	}
+
+	return result
 }
--- daily_challenge/divide_two_intergers_test.go
+++ daily_challenge/divide_two_intergers_test.go
@@ -6,10 +6,14 @@ func Test_Divide(t *testing.T) {
 	sourceCase := [][]int{
 		{10, 3},
 		{7, -3},
+		{-2147483648, -1},
+		{-2147483648, 2},
 	}
 	expectList := []int{
 		3,
 		-2,
+		2147483647,
+		-1073741824,
 	}
 
 	for i, source := range sourceCase {
