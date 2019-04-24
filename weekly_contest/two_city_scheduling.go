eba425d1b9d9c2d819aea45729d00141607c0a1f
liuwill <liuwei_will@qq.com>
2019-04-24 15:42:17 +0800

    finish Two City Scheduling with sort distance

--- weekly_contest/two_city_scheduling.go
+++ weekly_contest/two_city_scheduling.go
@@ -1,19 +1,58 @@
 package weekly_contest
 
+import (
+	"sort"
+)
+
+type DistanceList [][]int
+
+func (self DistanceList) Len() int {
+	return len(self)
+}
+
+func (self DistanceList) Less(i, j int) bool {
+	return self[i][1] > self[j][1]
+}
+
+func (self DistanceList) Swap(i, j int) {
+	self[i], self[j] = self[j], self[i]
+}
+
 /**
  * weekly-contest-1029
  * PUZZLE: Two City Scheduling
  */
 func twoCitySchedCost(costs [][]int) int {
 	target := 0
-	for i := 0; i < len(costs); i++ {
-		item := costs[i]
-		current := item[0]
-		if item[1] < item[0] {
-			current = item[1]
-		}
+	disList := make([][]int, len(costs))
+	count := len(costs) / 2
 
-		target += current
+	for i := 0; i < len(costs); i++ {
+		distance := costs[i][0] - costs[i][1]
+		if distance < 0 {
+			distance = costs[i][1] - costs[i][0]
+		}
+		disList[i] = []int{i, distance}
 	}
+
+	a := 0
+	b := 0
+	sort.Sort(DistanceList(disList))
+	// fmt.Printf("%v\n", disList)
+	for j := 0; j < len(disList); j++ {
+		current := disList[j]
+		item := costs[current[0]]
+		val := item[1]
+
+		if (count > a && item[0] < val) || b >= count {
+			val = item[0]
+			a++
+		} else {
+			b++
+		}
+		// println(current[0], val, count, a)
+		target += val
+	}
+
 	return target
 }
--- weekly_contest/two_city_scheduling_test.go
+++ weekly_contest/two_city_scheduling_test.go
@@ -5,9 +5,11 @@ import "testing"
 func Test_TwoCitySchedCost(t *testing.T) {
 	sourceCase := [][][]int{
 		[][]int{{10, 20}, {30, 200}, {400, 50}, {30, 20}},
+		[][]int{{259, 770}, {448, 54}, {926, 667}, {184, 139}, {840, 118}, {577, 469}},
 	}
 	expectList := []int{
 		110,
+		1859,
 	}
 
 	for i, source := range sourceCase {
