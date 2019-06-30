12cb9b37b348c66d507819c9fd96076b11560f1a
liuwill <liuwei_will@qq.com>
2019-06-30 15:49:43 +0800

    solve car pooling with more space than time

--- weekly_contest/car_pooling.go
+++ weekly_contest/car_pooling.go
@@ -3,5 +3,18 @@ package weekly_contest
 func carPooling(trips [][]int, capacity int) bool {
 	posData := make([]int, 1000)
 
+	for pos := 0; pos < len(trips); pos++ {
+		tripStep := trips[pos]
+		num := tripStep[0]
+		for i := tripStep[1]; i < tripStep[2]; i++ {
+			posData[i-1] += num
+		}
+	}
+
+	for num := range posData {
+		if num > capacity {
+			return false
+		}
+	}
 	return true
 }
