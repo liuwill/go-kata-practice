7df87ba5323a80c47ea6f40fe7b8e0d00c510746
liuwill <liuwei_will@qq.com>
2019-07-26 14:30:03 +0800

    fix test case and pass

--- weekly_contest/flight_bookings_test.go
+++ weekly_contest/flight_bookings_test.go
@@ -5,12 +5,15 @@ import "testing"
 func Test_CorpFlightBookings(t *testing.T) {
 	rootCase := [][][]int{
 		[][]int{{1, 2, 10}, {2, 3, 20}, {2, 5, 25}},
+		[][]int{{1, 2, 10}, {2, 3, 20}, {2, 4, 25}},
 	}
 	sourceCase := []int{
+		5,
 		5,
 	}
 	expectList := [][]int{
 		[]int{10, 55, 45, 25, 25},
+		[]int{10, 55, 45, 25, 0},
 	}
 
 	for i, source := range sourceCase {
@@ -18,7 +21,7 @@ func Test_CorpFlightBookings(t *testing.T) {
 		expect := expectList[i]
 
 		target := corpFlightBookings(root, source)
-		if compareList(target, expect) {
+		if !compareList(target, expect) {
 			t.Error("Run Test_CorpFlightBookings Fail", expect, target, root)
 		}
 	}
