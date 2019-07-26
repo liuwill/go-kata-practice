5bef3e290490518c556ade8a09bc17e2477bda67
liuwill <liuwei_will@qq.com>
2019-07-26 14:27:31 +0800

    solve Corporate Flight Bookings

--- weekly_contest/flight_bookings.go
+++ weekly_contest/flight_bookings.go
@@ -1,5 +1,13 @@
 package weekly_contest
 
 func corpFlightBookings(bookings [][]int, n int) []int {
+	result := make([]int, n)
 
+	for i := 0; i < len(bookings); i++ {
+		booking := bookings[i]
+		for j := booking[0] - 1; j < booking[1]; j++ {
+			result[j] += booking[2]
+		}
+	}
+	return result
 }
---
+++ weekly_contest/flight_bookings_test.go
@@ -0,0 +1,27 @@
+package weekly_contest
+
+import "testing"
+
+func Test_CorpFlightBookings(t *testing.T) {
+	rootCase := [][][]int{
+		[][]int{{1, 2, 10}, {2, 3, 20}, {2, 5, 25}},
+	}
+	sourceCase := []int{
+		5,
+	}
+	expectList := [][]int{
+		[]int{10, 55, 45, 25, 25},
+	}
+
+	for i, source := range sourceCase {
+		root := rootCase[i]
+		expect := expectList[i]
+
+		target := corpFlightBookings(root, source)
+		if compareList(target, expect) {
+			t.Error("Run Test_CorpFlightBookings Fail", expect, target, root)
+		}
+	}
+
+	t.Log("Run Test_CorpFlightBookings Success")
+}
