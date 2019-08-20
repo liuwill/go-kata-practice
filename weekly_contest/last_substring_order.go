ff112177fc50ba88bba93cd4ca655085ddb00f5b
liuwill <liuwei_will@qq.com>
2019-08-20 18:09:46 +0800

    coding finish not right

--- weekly_contest/last_substring_order.go
+++ weekly_contest/last_substring_order.go
@@ -1,6 +1,24 @@
 package weekly_contest
 
 func lastSubstring(s string) string {
+	big := 'a'
+	positions := []int{}
 
-	return s
+	for i, v := range s {
+		if v == big {
+			positions = append(positions, i)
+		} else if v > big {
+			positions = []int{i}
+		}
+	}
+
+	result := ""
+	for _, pos := range positions {
+		current := s[pos:]
+		if current > result {
+			result = current
+		}
+	}
+
+	return result
 }
---
+++ weekly_contest/last_substring_order_test.go
@@ -0,0 +1,25 @@
+package weekly_contest
+
+import "testing"
+
+func Test_LastSubstring(t *testing.T) {
+	mapCase := []string{
+		"abab",
+		"bab",
+	}
+	expectList := []string{
+		"leetcode",
+		"tcode",
+	}
+
+	for i, source := range mapCase {
+		expect := expectList[i]
+
+		target := lastSubstring(source)
+		if target != expect {
+			t.Error("Run Test_LastSubstring Fail", expect, target)
+		}
+	}
+
+	t.Log("Run Test_LastSubstring Success")
+}
