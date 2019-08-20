1ece1b5423d6a0e6eca41a63d716bcffee76dd2c
liuwill <liuwei_will@qq.com>
2019-08-20 16:04:15 +0800

    try wrong far from land possible

--- weekly_contest/far_land_possible.go
+++ weekly_contest/far_land_possible.go
@@ -1,5 +1,40 @@
 package weekly_contest
 
+func farLandDistance(x []int, y []int) int {
+	long := y[1] - x[1]
+	width := y[0] - x[0]
+
+	if long < 0 {
+		long = -long
+	}
+	if width < 0 {
+		width = -width
+	}
+
+	return long + width
+}
+
 func maxDistance(grid [][]int) int {
-	return 0
+	max := 0
+	for i, line := range grid {
+		for j, v := range line {
+			if v == 1 {
+				continue
+			}
+
+			for ii, line := range grid {
+				for jj, v := range line {
+					if v == 0 {
+						continue
+					}
+
+					distance := farLandDistance([]int{i, j}, []int{ii, jj})
+					if distance > max {
+						max = distance
+					}
+				}
+			}
+		}
+	}
+	return max
 }
---
+++ weekly_contest/far_land_possible_test.go
@@ -0,0 +1,22 @@
+package weekly_contest
+
+import "testing"
+
+func Test_MaxDistance(t *testing.T) {
+	mapCase := [][][]int{
+		{{1, 0, 1}, {0, 0, 0}, {1, 0, 1}},
+		{{1, 0, 0}, {0, 0, 0}, {0, 0, 0}},
+	}
+	expectList := []int{2, 4}
+
+	for i, source := range mapCase {
+		expect := expectList[i]
+
+		target := maxDistance(source)
+		if target != expect {
+			t.Error("Run Test_MaxDistance Fail", expect, target)
+		}
+	}
+
+	t.Log("Run Test_MaxDistance Success")
+}
