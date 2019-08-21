f412a2758c36bb4b23bacacd3179a26e0b949ed5
liuwill <liuwei_will@qq.com>
2019-08-21 14:47:28 +0800

    almost finish but debuging

--- weekly_contest/find_words_count_characters.go
+++ weekly_contest/find_words_count_characters.go
@@ -1,5 +1,30 @@
 package weekly_contest
 
 func countCharacters(words []string, chars string) int {
-	return len(words)
+	letterMap := make(map[rune]int)
+
+	for _, c := range chars {
+		if _, ok := letterMap[c]; !ok {
+			letterMap[c] = 0
+		}
+		letterMap[c]++
+	}
+
+	count := 0
+	for _, word := range words {
+		match := 0
+		for _, l := range word {
+			if _, ok := letterMap[l]; !ok || letterMap[l] <= 0 {
+				match = 0
+				break
+			}
+
+			match++
+			letterMap[l]--
+		}
+
+		count += match
+	}
+
+	return count
 }
