910eb68beea09beaf1f46f9a5a27bfc24ca294a5
liuwill <liuwei_will@qq.com>
2019-09-04 17:48:23 +0800

    find but too slow

--- weekly_contest/find_num_valid_words.go
+++ weekly_contest/find_num_valid_words.go
@@ -1,5 +1,37 @@
 package weekly_contest
 
+func initPuzzle(puzzle string) []int {
+	list := make([]int, 26)
+	for _, l := range puzzle {
+		list[l-97] = 1
+	}
+	return list
+}
+
 func findNumOfValidWords(words []string, puzzles []string) []int {
-	return []int{}
+	target := make([]int, len(puzzles))
+	for i, puzzle := range puzzles {
+		pMap := initPuzzle(puzzle)
+		first := rune(puzzle[0])
+
+		for _, word := range words {
+			head := false
+			match := true
+			for _, l := range word {
+				if l == first {
+					head = true
+				}
+
+				if pMap[l-97] != 1 {
+					match = false
+					break
+				}
+			}
+
+			if head && match {
+				target[i]++
+			}
+		}
+	}
+	return target
 }
