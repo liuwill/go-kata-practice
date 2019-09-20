38d7ccad29d11ec2e982be186c550420754af66a
liuwill <liuwei_will@qq.com>
2019-09-20 13:51:45 +0800

    fisnish Distance Between Bus Stops

--- weekly_contest/distance_between_bus_stops.go
+++ weekly_contest/distance_between_bus_stops.go
@@ -1,5 +1,20 @@
 package weekly_contest
 
 func distanceBetweenBusStops(distance []int, start int, destination int) int {
-	return start
+	total := 0
+	min := 0
+
+	for i, v := range distance {
+		total += v
+		if i >= start && i < destination {
+			min += v
+		}
+	}
+
+	guess := total - min
+	if min > guess {
+		min = guess
+	}
+
+	return min
 }
