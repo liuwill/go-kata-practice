3d3b2ea2eed5dfcd8c1f0ec16212718ff73205eb
liuwill <liuwei_will@qq.com>
2019-09-20 13:56:14 +0800

    avoid start big then destination

--- weekly_contest/distance_between_bus_stops.go
+++ weekly_contest/distance_between_bus_stops.go
@@ -1,8 +1,18 @@
 package weekly_contest
 
+/**
+ * daily-challenge-1184
+ * PUZZLE: Distance Between Bus Stops
+ */
 func distanceBetweenBusStops(distance []int, start int, destination int) int {
 	total := 0
 	min := 0
+
+	big := destination
+	if start > big {
+		destination = start
+		start = big
+	}
 
 	for i, v := range distance {
 		total += v
