6008569342a3d0a16103fd9ce2d77d099936e80c
liuwill <liuwei_will@qq.com>
2019-09-28 19:59:33 +0800

    finish 1200. Minimum Absolute Difference

--- weekly_contest/minimum_abs_difference.go
+++ weekly_contest/minimum_abs_difference.go
@@ -1,5 +1,30 @@
 package weekly_contest
 
+import "sort"
+
+const MAX = 1000000
+
+/**
+ * daily-challenge-1200
+ * PUZZLE: Minimum Absolute Difference
+ */
 func minimumAbsDifference(arr []int) [][]int {
-	return nil
+	sort.Ints(arr)
+	subList := make([]int, len(arr)-1)
+	min := MAX
+	for i := 0; i < len(arr)-1; i++ {
+		subList[i] = arr[i+1] - arr[i]
+		if subList[i] < min {
+			min = subList[i]
+		}
+	}
+
+	target := [][]int{}
+	for i, val := range subList {
+		if val == min {
+			target = append(target, []int{arr[i], arr[i+1]})
+		}
+	}
+
+	return target
 }
