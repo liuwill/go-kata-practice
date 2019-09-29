9e84ff702d7a833d828e34faa79f69a7d106e068
liuwill <liuwei_will@qq.com>
2019-09-29 17:03:16 +0800

    finish uniqueOccurrences

--- weekly_contest/unique_occurrences.go
+++ weekly_contest/unique_occurrences.go
@@ -1,5 +1,20 @@
 package weekly_contest
 
 func uniqueOccurrences(arr []int) bool {
-	return false
+	result := make([]int, 1001)
+	dict := make(map[int]int)
+
+	for _, v := range arr {
+		if _, ok := dict[v]; !ok {
+			dict[v] = 0
+		}
+		dict[v]++
+	}
+	for _, v := range dict {
+		if result[v] > 0 {
+			return false
+		}
+	}
+
+	return true
 }
