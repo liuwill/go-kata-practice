83e03046ed3eb92819d37f3f5289085fa4d7d85b
liuwill <liuwei_will@qq.com>
2019-10-14 13:55:49 +0800

    finish balancedStringSplit

--- weekly_contest/balanced_string_split.go
+++ weekly_contest/balanced_string_split.go
@@ -1,5 +1,22 @@
 package weekly_contest
 
 func balancedStringSplit(s string) int {
-	return len(s)
+	llen := 0
+	rlen := 0
+
+	count := 0
+	for _, v := range s {
+		if v == 'L' {
+			llen++
+		} else if v == 'R' {
+			rlen++
+		}
+
+		if llen == rlen {
+			llen = 0
+			rlen = 0
+			count++
+		}
+	}
+	return count
 }
