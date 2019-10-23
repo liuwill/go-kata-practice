c53bcf90463f2d73aa3e60f359e2fa8a44fcb6da
liuwill <liuwei_will@qq.com>
2019-10-23 16:36:03 +0800

    finish first version sub folder remove

--- weekly_contest/remove_sub_folders.go
+++ weekly_contest/remove_sub_folders.go
@@ -1,5 +1,55 @@
 package weekly_contest
 
+func compareSubFolder(first string, second string) int {
+	long := first
+	short := second
+	position := true
+
+	if len(second) > len(first) {
+		long = second
+		short = first
+
+		position = false
+	}
+
+	for i, _ := range short {
+		if long[i] != short[i] {
+			return 0
+		}
+	}
+
+	if position {
+		return -1
+	}
+
+	return 1
+}
+
 func removeSubfolders(folder []string) []string {
-	return nil
+	mark := make([]int, len(folder))
+	for i := 0; i < len(folder)-1; i++ {
+		if mark[i] == 1 {
+			continue
+		}
+		for j := i + 1; j < len(folder); j++ {
+			if mark[j] == 1 {
+				continue
+			}
+
+			compare := compareSubFolder(folder[i], folder[j])
+			if compare == -1 {
+				mark[i] = 1
+			} else if compare == 1 {
+				mark[j] = 1
+			}
+		}
+	}
+
+	result := []string{}
+	for i, v := range mark {
+		if v == 0 {
+			result = append(result, folder[i])
+		}
+	}
+	return result
 }
