e1b16b82cdad9d0100d19b626b6acbc36b8cc3fc
liuwill <liuwei_will@qq.com>
2019-10-28 11:20:06 +0800

    finish checkStraightLine without border

--- weekly_contest/check_straight_line.go
+++ weekly_contest/check_straight_line.go
@@ -1,5 +1,16 @@
 package weekly_contest
 
+func tanStraightLine(start []int, end []int) int {
+	return (start[0] - end[0]) / (start[1] - end[1])
+}
+
 func checkStraightLine(coordinates [][]int) bool {
-	return false
+	tan := tanStraightLine(coordinates[0], coordinates[1])
+	for i := 1; i < len(coordinates)-1; i++ {
+		current := tanStraightLine(coordinates[i], coordinates[i+1])
+		if tan != current {
+			return false
+		}
+	}
+	return true
 }
