071e40b5860e9a9ca0bbe3bb7b4bf9b40f68093f
liuwill <liuwei_will@qq.com>
2019-11-05 14:07:52 +0800

    finish min remove to make valid parenthese

--- weekly_contest/min_remove_make_valid.go
+++ weekly_contest/min_remove_make_valid.go
@@ -1,5 +1,43 @@
 package weekly_contest
 
 func minRemoveToMakeValid(s string) string {
-	return s
+	ll := len(s)
+	targetMap := make([]int, ll)
+	stack := make([]int, ll)
+	top := -1
+
+	tll := 0
+	for i, v := range s {
+		if v != '(' && v != ')' {
+			targetMap[i] = 1
+			tll++
+			continue
+		}
+
+		if v == '(' {
+			top++
+			stack[top] = i
+		} else if v == ')' {
+			if top < 0 {
+				continue
+			}
+
+			tt := stack[top]
+			targetMap[i] = 1
+			targetMap[tt] = 1
+			tll += 2
+			top--
+		}
+	}
+
+	runeStr := make([]rune, tll)
+	rll := 0
+	for i, v := range s {
+		if targetMap[i] == 1 {
+			runeStr[rll] = v
+			rll++
+		}
+	}
+
+	return string(runeStr)
 }
