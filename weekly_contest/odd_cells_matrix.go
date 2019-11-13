1edf7572bd83058ee4724964537236fdc0b5e336
liuwill <liuwei_will@qq.com>
2019-11-13 09:59:54 +0800

    coding odd cell simple for matrix

--- weekly_contest/odd_cells_matrix.go
+++ weekly_contest/odd_cells_matrix.go
@@ -1,5 +1,29 @@
 package weekly_contest
 
 func oddCells(n int, m int, indices [][]int) int {
-	return 0
+	matrix := make([][]int, n)
+	for i, _ := range matrix {
+		matrix[i] = make([]int, m)
+	}
+
+	for _, v := range indices {
+		for i := 0; i < m; i++ {
+			matrix[v[0]][i]++
+		}
+
+		for j := 0; j < n; j++ {
+			matrix[j][v[1]]++
+		}
+	}
+
+	count := 0
+	for _, line := range matrix {
+		for _, item := range line {
+			if item%2 == 1 {
+				count++
+			}
+		}
+	}
+
+	return count
 }
