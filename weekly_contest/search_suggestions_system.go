0ec5cd99ae313a6f9a00256b24847f75267d4c28
liuwill <liuwei_will@qq.com>
2019-11-28 10:21:31 +0800

    first try suggest products

--- weekly_contest/search_suggestions_system.go
+++ weekly_contest/search_suggestions_system.go
@@ -1,5 +1,32 @@
 package weekly_contest
 
-func suggestedProducts(products []string, searchWord string) [][]string {
+import "strings"
 
+func suggestedProducts(products []string, searchWord string) [][]string {
+	ll := len(searchWord)
+
+	target := make([][]string, ll)
+	left := make([]string, len(products))
+	for i, v := range products {
+		left[i] = v
+	}
+	for i, _ := range searchWord {
+		current := searchWord[0 : i+1]
+		filtered := []string{}
+		for _, item := range left {
+			if strings.HasPrefix(item, current) {
+				filtered = append(filtered, item)
+			}
+		}
+		left = filtered
+
+		l := len(filtered)
+		if l > 3 {
+			l = 3
+		}
+
+		target[i] = filtered[0:l]
+	}
+
+	return target
 }
