f89c4b833079dbf0d5fab8bbaa7a759ac390f5fd
liuwill <liuwei_will@qq.com>
2019-11-29 11:09:40 +0800

    finish count server coding

--- weekly_contest/count_servers.go
+++ weekly_contest/count_servers.go
@@ -1,5 +1,44 @@
 package weekly_contest
 
 func countServers(grid [][]int) int {
-	return 0
+	count := 0
+	m := len(grid)
+	n := len(grid[0])
+
+	source := make([][]int, m)
+	for i, line := range grid {
+		source[i] = line[:]
+	}
+
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if source[i][j] == 0 {
+				continue
+			}
+
+			if i-1 >= 0 && source[i-1][j] > 0 {
+				source[i][j]++
+				count++
+				continue
+			}
+			if j-1 >= 0 && source[i][j-1] > 0 {
+				source[i][j]++
+				count++
+				continue
+			}
+
+			if i+1 < m && source[i+1][j] > 0 {
+				source[i][j]++
+				count++
+				continue
+			}
+
+			if j+1 < n && source[i][j+1] > 0 {
+				source[i][j]++
+				count++
+				continue
+			}
+		}
+	}
+	return count
 }
